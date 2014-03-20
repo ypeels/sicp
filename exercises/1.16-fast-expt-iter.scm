1ef18ba7b915a7355ebed0c8ff8859bbdbe5aa19
Jonathan Chen (home) <chen@chinstruments.com>
2014-03-19 22:52:06 -0500

    Exercise 1.16 - ohhh I had to sit down and REALLY work it out on pen/paper; after that, it was just a couple minutes of coding

--- exercises/1.16-fast-expt-iter.scm
+++ exercises/1.16-fast-expt-iter.scm
@@ -2,49 +2,46 @@
 ; (square x)
 ; (remainder x m)
 
-; isn't this just a trivial modification of fast-expt?
-; actually, it's not. i blundered upon the solution without having the right derivation...? 
-; or just the right intuition but not the math?
+; isn't this just a trivial modification of fast-expt? NO...
 
-; Let C := b^N be the loop invariant.
-; C = (b^a)^n       defines the state variables.
+; ohhhhh here's what they meant by the hint:
+; Let C = b^N
+; C = a b^n defines the loop invariant from the problem.
+; Let a = b^j to symmetrize.
+; a = 1 and thus j = 0 initially.
 
-; C = (b^1)^N           initial state variables a = b, n = N. (b is a constant)
-; C = (b^2)^(N/2)       a = b^2, n = N/2
-; C = (b^4)^(N/4)       a = b^4, n = N/4. now suppose N/4 is ODD.
-; C = (b^5)^[(N/4)-1]   a = b^5, n = N/4 - 1. now n is EVEN, and you can square up again!
-; ok, with the loop invariant on solid footing, i feel much happier putting it in code
+; C = b^j b^n for n odd
+; C = b^(j+1) b^(n-1)
 
-;(define (fast-expt-iterative b N)
-;    (define (iter a b k)
-;        (cond 
-;        
-;            ; "loop" termination (includes trivial case)
-;            ((= k 0) a)
-;            
-;            ; first iteration OR odd k
-;            ((or (= a 1) (odd? k))
-;                (iter
-;                    (* a b)         ; a *= b
-;                    b
-;                    (- k 1)))       ; loop invariant: a b^(k-1) = b^n (scheme is case INSENSITIVE!? come ON)    
-;            
-;            ((even? n) 
-;                (iter 
-;                    (square a)      ; a = a^2
-;                    b
-;                    (/ k 2)         ; before, invariant = b^j b^k = b^n; after: invariant = b^2j b^k/2
-;            
-;            ; there should be no other cases...
-;            (else -1)
-;            
-;            
-;        
-;        
-;        
-;        ; if n == 0:
-;        ;   return a
-;        ; elif 
+; What does the even case mean? (Still following the Hint)
+; C = b^j b^n
+; C = b^j (b^2)^(n/2). AHA, you need to SQUARE THE BASE, not the accumulator!!!
+
+; termination
+; C = b^j b^1 - just another odd case; it'll terminate when n == 0
+
+
+(define (fast-expt-iterative base exponent)    
+
+    (define (iter a b n)
+        (cond 
+            ((<= n 0) a)        ; "loop termination"         
+        
+            ((even? n)                
+                (iter           
+                    a           ; a = a
+                    (square b)  ; b = b**2
+                    (/ n 2)))   ; n = n/2
+                    
+            (else  ; n is odd
+                (iter
+                    (* a b)     ; a = a*b
+                    b           ; b = b
+                    (- n 1))))) ; n = n-1
+              
+           
+    (iter 1 base exponent))
+
         
         
         
@@ -54,3 +51,53 @@
     
 (define (odd? n)
     (not (even? n)))
+
+; for testing
+(define (f b e) (fast-expt-iterative b e))
+
+
+
+
+; actually, it's not. i blundered upon the solution without having the right derivation...? 
+; or just the right intuition but not the math?
+
+; Let C := b^N be the loop invariant.
+; C = (b^a)^n       defines the state variables.
+
+; C = (b^1)^N           initial state variables a = b, n = N. (b is a constant)
+; C = (b^2)^(N/2)       a = b^2, n = N/2
+; C = (b^4)^(N/4)       a = b^4, n = N/4. now suppose N/4 is ODD.
+; C = (b^5)^[(N/4)-1]   - NO, THIS STEP IS WRONG. the math doesn't add up!!!
+; also, this FORM of the loop invariant is not following the Hint...
+
+
+; except this algorithm is WRONG FOR ODD N...
+; try N = 3 case
+; C = (b^1)^3
+; C = (b 
+        ; this was wrong
+        ;    ; "loop initialization" - kinda nontrivial
+        ;    ((= a 1)   ; n exponent)  no, can't say n == exponent case is NECESSARILY the first iteration...
+        ;        (cond
+        ;            ; corner case AND invalid input...
+        ;            ((<= exponent 0) 1)  
+        ;            
+        ;            ; exponent > 1, so seed the process... otherwise b^(2^k) will just return 1...
+        ;            (else (iter b b n))))
+        ;    
+        ;    ; "loop" termination       
+        ;    ;((= n 0) a) 
+        ;    
+        ;    ((= n 1) a) ; "loop" termination       
+        ;    
+        ;    ((even? n) 
+        ;        (iter 
+        ;            (square a)      ; a *= a
+        ;            b
+        ;            (/ n 2)))       ; n /= 2
+        ;    
+        ;    (else  ; odd n > 1
+        ;        (iter
+        ;            (* a b)         ; a *= b
+        ;            b
+        ;            (- n 1)))))     ; n -= 1
