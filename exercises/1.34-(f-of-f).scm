961d520aa2733e9727d410f567f640b67aeda2c5
Jonathan Chen (work) <chen@chinstruments.com>
2014-03-21 14:41:31 -0500

    Exercise 1.34 - wow, finally a brief break in the exercises

---
+++ exercises/1.34-(f-of-f).scm
@@ -0,0 +1,8 @@
+; wow, the first subsection in a WHILE that has had a SHORT set of exercises
+
+(define (f g) (g 2))
+(display (f f))         ; The object 2 is not applicable
+; (f f)
+; (f 2)
+; (2 2)
+; well that was easy...
