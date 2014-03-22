ea728c1cc265f6f0e8fa4b69811f6515ea38bdf3
Jonathan Chen (home) <chen@chinstruments.com>
2014-03-22 14:22:37 -0500

    removed some unneeded comments that were copy/pasted from ch1.scm

--- exercises/1.46-iterative-improve.scm
+++ exercises/1.46-iterative-improve.scm
@@ -54,18 +54,6 @@
         ((iterative-improve good-enough? f) initial-guess)
     )
 )
-    
-    ; from ch1.scm
-;(define tolerance 0.00001)
-;(define (fixed-point f first-guess)
-;  (define (close-enough? v1 v2)
-;    (< (abs (- v1 v2)) tolerance))
-;  (define (try guess)
-;    (let ((next (f guess)))
-;      (if (close-enough? guess next)
-;          next
-;          (try next))))
-;  (try first-guess))
 
 
 (define (test-1.46)
