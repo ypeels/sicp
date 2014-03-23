2320106ef72009a609c7c7c8c4ddc6719b822ceb
Jonathan Chen (home) <chen@chinstruments.com>
2014-03-23 12:39:23 -0500

    extended the Segment API

--- exercises/2.02-make-point.scm
+++ exercises/2.02-make-point.scm
@@ -31,6 +31,39 @@
 (define (end-segment s) (cdr s))
 (define (midpoint-segment s) 
     (midpoint (start-segment s) (end-segment s)))
+    
+
+
+; added for exercise 2.3  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  
+;(define (x-distance-segment s) 
+;    (abs (- (x-point (start-segment s)) (x-point (end-segment s))))
+;(define (y-distance-segment s) 
+;    (abs (- (y-point (start-segment s)) (y-point (end-segment s))))
+    
+(define (x-distance-segment s) (axis-distance-segment s x-point))
+(define (y-distance-segment s) (axis-distance-segment s y-point)) ; is this more "Scheming"? at least it's DRYer
+(define (axis-distance-segment s axis-point)
+    (abs (- (axis-point (start-segment s)) (axis-point (end-segment s)))))
+
+; some convenience functions
+(define (is-horizontal-segment? s) (= 0 (y-distance s)))
+(define (is-vertical-segment? s) (= 0 (x-distance s)))
+
+(define (length-segment s)  
+    ;(let (  (x1 (x-point (start-segment s))) (y1 (y-point (start-segment s)))
+    ;        (x2 (x-point (end-segment s))) (y2 (y-point (end-segment s))))
+    ;    (cond  
+    ;        ((= x1 x2) (abs (- y1 y2)))
+    ;        ((= y1 y2) (abs (- x1 x2)))
+    ;        (else (error "diagonal segment" x1 x2 y1 y2)))
+    ;)
+    
+    (cond 
+        ((is-horizontal-segment? s) (y-distance s))
+        ((is-vertical-segment? s)   (x-distance s))
+        (else (error "unimplemented: diagonal segment")))
+)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ; Test code
@@ -51,4 +84,4 @@
     (test -4 0 0 -2)
 )
 
-; (test-2.2)
+(test-2.2)
