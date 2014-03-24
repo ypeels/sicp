0ceb5a22e179e5370e2dc0de92dc0380c9299100
Jonathan Chen (netbook) <ypeels@live.com>
2014-03-24 09:12:58 -0500

    Exercise 2.8 finished with my answer... how to check? meh

--- exercises/2.08-sub-interval.scm
+++ exercises/2.08-sub-interval.scm
@@ -1,5 +1,13 @@
 ; Using reasoning analogous to Alyssa's, describe how the difference of two intervals may be computed. 
-(define (sub-interval
+(define (sub-interval x y)
+    (let (  (p1 (- (lower-bound x) (lower-bound y)))
+            (p2 (- (lower-bound x) (upper-bound y)))
+            (p3 (- (upper-bound x) (lower-bound y)))
+            (p4 (- (upper-bound x) (upper-bound y))))
+        (make-interval (min p1 p2 p3 p4) (max (p1 p2 p3 p4)))
+    )
+)
+        
 
     ; use 4 cases like mul-interval, cuz MAYBE there are NEGATIVE numbers here...
     ; ADDITION is the special case that doesn't need min/max testing...
