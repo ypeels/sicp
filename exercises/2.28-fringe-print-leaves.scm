133bc7813583a77deb1fed13358cabd23e159b55
Jonathan Chen (work) <chen@chinstruments.com>
2014-03-25 13:37:29 -0500

    Exercise 2.28 - passes all tests from the book, so that's it for me...

---
+++ exercises/2.28-fringe-print-leaves.scm
@@ -0,0 +1,27 @@
+; I'm not quite sure what the problem is asking for...
+; I'll just stop once the listed examples run correctly
+
+
+(define (fringe tree)
+
+    (cond 
+        ((null? tree) ())                     ; exploit: (append list ()) = list
+        ((not (pair? tree)) (list tree))
+        (else (append (fringe (car tree)) (fringe (cdr tree))))
+    )
+)
+
+        
+    
+        
+
+
+
+(define (test-2.28)
+    (let ((x (list (list 1 2) (list 3 4))))
+        (newline) (display (fringe x))  ; should be (1 2 3 4)
+        (newline) (display (fringe (list x x)))
+    )
+)
+
+; (test-2.28)
