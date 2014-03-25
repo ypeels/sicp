21a9ea0b1468d8d13ecc0e60f2e1ee5fde890ec8
Jonathan Chen (work) <chen@chinstruments.com>
2014-03-25 11:12:12 -0500

    Exercise 2.21 - (map) vs manual 'iteration'

---
+++ exercises/2.21-square-list.scm
@@ -0,0 +1,31 @@
+(define nil ()) ; apparently MIT Scheme 9.1 needs this
+
+(define (square-list items)
+  (if (null? items)
+      nil
+      (cons 
+        (square (car items))        ; <??>                
+        (square-list (cdr items))   ; <??>
+      )
+  )
+)
+
+(define (square-list-map items)
+  (map 
+    (lambda (x) (square x))         ; <??> 
+    items                           ; <??>
+  )
+)
+
+
+(define (test-2.21)
+    (define (test procedure)
+        (newline)
+        (display (procedure (list 1 2 3 4)))    ; should be (1 4 9 16)
+    )
+
+    (test square-list)
+    (test square-list-map)
+)
+
+; (test-2.21)
