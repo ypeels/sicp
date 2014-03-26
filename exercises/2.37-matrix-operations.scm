1471a2bd740141276a09f6848818a4b864fd38b4
Jonathan Chen (work) <chen@chinstruments.com>
2014-03-26 13:03:54 -0500

    Exercise 2.37 all working, with development notes

--- exercises/2.37-matrix-operations.scm
+++ exercises/2.37-matrix-operations.scm
@@ -15,9 +15,23 @@
 
 (define (transpose mat)
   (accumulate-n 
-    cons    ; <??> operation. this works because (accumulate) operates from RIGHT TO LEFT
-    ()      ; <??> initial value 
+    cons                            ; <??> operation. this works out of the box because (accumulate) operates from RIGHT TO LEFT
+    ()                              ; <??> initial value 
     mat
+  )
+)
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map                            ; for each ROW of m, apply <??>, which returns a new row (unless there's recursion...)     
+        
+        ; how about we "cheat" and add ANOTHER ARGUMENT to map? no, would only compute diagonal elements...
+        
+        ; <??> WANT to multiply current row with the kth column...
+        (lambda (row) (map (lambda (col) (dot-product row col)) cols))
+        
+        m
+    )
   )
 )
   
@@ -33,10 +47,11 @@
          (display "\nv.v: ") (display (dot-product v v))
          (display "\nm*v: ") (display (matrix-*-vector m v))
          (display "\nm': ") (display (transpose m))
+         (display "\nm*m: ") (display (matrix-*-matrix m m))
     )
         
     
-    (test (list 1 1 1 1) (list (list 1 2 3 4) (list 4 5 6 6) (list 6 7 8 9)))
+    (test (list 1 1 1 1) (list (list 1 2 3 4) (list 4 5 6 6) (list 6 7 8 9) (list 0 0 0 0)))
     (test (list 1 2 3) (list (list 1 0 0) (list 0 1 0) (list 0 0 1)))
     (test (list 1 2 3) (list (list 0 0 1) (list 0 1 0) (list 1 0 0)))
 
