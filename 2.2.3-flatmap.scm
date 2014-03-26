2c61eb995c6d12d90a99841a6be223e8b9659d62
Jonathan Chen (work) <chen@chinstruments.com>
2014-03-26 18:46:39 -0500

    copied in their (permutations) code, to be annotated

--- 2.2.3-flatmap.scm
+++ 2.2.3-flatmap.scm
@@ -71,7 +71,19 @@
           sequence))
                 
 
+                
+(define (permutations s)
+  (if (null? s)                         ; empty set?
+      (list nil)                        ; sequence containing empty set
+      (flatmap (lambda (x)
+                 (map (lambda (p) (cons x p))
+                      (permutations (remove x s))))
+               s)))
 
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item)))
+          sequence))
+                
   
   
 
