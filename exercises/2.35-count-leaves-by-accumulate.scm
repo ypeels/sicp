dc43f70e1a7402bdc30ff51c602d03010ac4b4d6
Jonathan Chen (netbook) <ypeels@live.com>
2014-03-26 10:43:49 -0500

    recursive version with my development dead ends

--- exercises/2.35-count-leaves-by-accumulate.scm
+++ exercises/2.35-count-leaves-by-accumulate.scm
@@ -14,6 +14,52 @@
   )
 )
 
+; http://community.schemewiki.org/?sicp-ex-2.35
+; "Using recursion, it's possible to do it with map and without enumerate-tree"
+; I caught a glimpse of their solution but didn't look too closely.
+; I guess I should have considered reaching back to that "Mapping over trees" section...
+; Here's what I worked out, using the "+ 0" as a starting point.
+(define (count-leaves-accum-recur t)
+    (accumulate
+        +                                   ; <??> accumulate operation. this was my first instinct
+        0                                   ; <??> initial value
+        (map
+            (lambda (subtree)
+                ;(display "\n\nlambda ") (display subtree)
+                ;(newline) (display (car subtree))
+                ;(newline) (display (cdr subtree))
+                
+                ; actually, it's (fringe) you should be ripping off, NOT (scale-tree)!
+                ;(cond 
+                ;    ((null? subtree) 0)         ; ignore null leaves
+                ;    ((not (pair? subtree)) 1)   ; count non-null leaves
+                ;    ((pair? subtree)            ; not a leaf, so recurse
+                ;        (+  (count-leaves-accum-recur (car subtree))
+                ;            (count-leaves-accum-recur (cdr subtree)))
+                ;    )
+                ;)
+                        
+                
+                ;(if (pair? subtree)         ; not a leaf
+                ;    (+  (count-leaves-accum-recur (list (car subtree)))
+                ;        (count-leaves-accum-recur (list (cdr subtree))))
+                ;    1
+                ;)
+                
+                ; oh, remember that map TAKES CARE OF TRAVERSAL FOR YOU
+                ; and because the (map) is wrapped with an (accum), you are SUMMING OVER THE TREE, not rebuilding it
+                (if (pair? subtree)
+                    (count-leaves-accum-recur subtree)
+                    1
+                )
+                
+            )
+            t
+        )
+    )
+)
+                    
+
 
 
 (define (test-2.35)
@@ -22,6 +68,7 @@
         (newline) (display y)
         (display "\noriginal: ") (display (count-leaves y))
         (display "\naccum: ") (display (count-leaves-accum y))
+        (display "\naccum recursive: ") (display (count-leaves-accum-recur y))
     )
     
     ; from section 2.2.2
