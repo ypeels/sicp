4f638651fa172bb484bc846dfbaebb36392f75ad
Jonathan Chen (netbook) <ypeels@live.com>
2014-03-26 12:05:28 -0500

    matrix * vector was easy enough

--- exercises/2.37-matrix-operations.scm
+++ exercises/2.37-matrix-operations.scm
@@ -7,7 +7,11 @@
   (accumulate + 0 (map * v w)))
   
 (define (matrix-*-vector m v)
-  (map <??> m))
+  (map 
+    (lambda (w) (dot-product v w))  ; <??> - binding v from procedure argument 
+    m
+  )
+)
   
   
   
@@ -16,13 +20,17 @@
 
          
     (define (test v m)    
-         (display "\nv: ") (display v)
+         (display "\n\nv: ") (display v)
          (display "\nm: ") (display m)
          (display "\nv.v: ") (display (dot-product v v))
+         (display "\nm*v: ") (display (matrix-*-vector m v))
     )
         
     
     (test (list 1 1 1 1) (list (list 1 2 3 4) (list 4 5 6 6) (list 6 7 8 9)))
+    (test (list 1 2 3) (list (list 1 0 0) (list 0 1 0) (list 0 0 1)))
+    (test (list 1 2 3) (list (list 0 0 1) (list 0 1 0) (list 1 0 0)))
+
 )
         
 (test-2.37)
