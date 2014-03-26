16269ee7ea033d714d2c56e811023ceb280b5db9
Jonathan Chen (netbook) <ypeels@live.com>
2014-03-26 10:17:07 -0500

    Exercise 2.35 - 'submission-clean'

--- exercises/2.35-count-leaves-by-accumulate.scm
+++ exercises/2.35-count-leaves-by-accumulate.scm
@@ -1,57 +1,15 @@
 (load "2.33-37-accumulate.scm")
-(load "2.28-fringe-print-leaves.scm") ;(define enumerate-tree fringe) ; for my cop-out...
-
-(define (debug x) "do nothing") 
-;(define debug display)
-
-
-(define (enumerate-tree t)
-    (debug "\nenumeratetree\n")
-    (let ((L (fringe t)))
-        (debug L)
-        L
-    )
-)
-
+(load "2.28-fringe-print-leaves.scm") (define enumerate-tree fringe) ; for my "cop-out"...
 
 
 (define (count-leaves-accum t)
   (accumulate 
+    (lambda (L sum) (+ sum (length L)))     ; <??> accumulate operation
+    0                                       ; <??> initial value
     
-    ; <??> accumulate operation
-    (lambda (L x) ; damn you, backwards (accumulate) api
-        (let ((count (accumulate 
-                        ;+ ; this would SUM all the items in L
-                        (lambda (y sum) (+ sum 1)) ; this would look better as a separate function, but that wouldn't satisfy the <??> form
-                        0 L)))
-                        
-            (debug count) (debug "\n")   
-            (+ x count)
-        ) ; x += sum(L)
-    )
-    
-   
-            
-           
-            
-    
-    
-    0           ; <??> initial value
-
-
-        
-    ; maybe map the tree onto a list? or onto a tree of lengths?
-    (map        ; target sequence for accumulate
-        ; attempted cop out, and does not work.
-        ;(lambda (t) (count-leaves-accum t)) 
-        
-        
-        ; can i just cop out and use "fringe"?
-        (lambda (t) (enumerate-tree t)); <??> map operation.
-        
-        t                                   ; <??> target sequence for map
-        
-        
+    (map                                    ; flatten the tree into a list, which accumulate can then consume
+        (lambda (t) (enumerate-tree t))         ; <??> map operation. not a cop out, is it? i mean, they did it in (sum-odd-squares)...
+        t                                       ; <??> target sequence for map
     )
   )
 )
@@ -75,8 +33,8 @@
 
     
     (let ((x (cons (list 1 2) (list 3 4))))
-        (test x)
-        (test (list x x))
+        (test x)            ; 4 leaves
+        (test (list x x))   ; 8 leaves
     )
 )
             
