58325fb2902adb65fdb4733dfc4d1ddb82f953df
Jonathan Chen (work) <ypeels@live.com>
2014-03-27 18:07:52 -0500

    Exercise 2.55 - one more funny case, nesting (quote) in true Scheming style

---
+++ exercises/2.55-car-''abracadabra.scm
@@ -0,0 +1,5 @@
+(display (car ''abracadabra))   ; quote
+    ; ''abracadabra
+    ; = (quote (quote abracadabra))
+    ; = '(quote abracadabra)
+    ; = ('quote 'abracadabra). it all still makes perfect sense using my notes from section 2.3.1
