56be1f98ba715e7cde5ff9b3d723f8fdb22230fa
Jonathan Chen (work) <ypeels@live.com>
2014-03-27 11:18:21 -0500

    tried doing the exercise with ugly ugly lambdas

--- exercises/2.41-unique-triples.scm
+++ exercises/2.41-unique-triples.scm
@@ -1,7 +1,5 @@
 (load "2.40-unique-pairs.scm")
 
-; k > i > j
-; this is due to the unfortunate convention used in (prime-sum-pairs)
 (define (unique-triples-with-k k)
 
     (define (triple-with-k pair)    ; analagous to (pair-with-i)
@@ -9,7 +7,7 @@
         
     (map 
         triple-with-k
-        (unique-pairs (- k 1))      ; ahhhh now isn't that clean!?
+        (unique-pairs (- k 1))      ; ahhhh now isn't that clean!? 
     )
 )
 
@@ -48,9 +46,90 @@
 
 
 
-(define (test-2.41)
-    (display (unique-triples-with-sum 10 21))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; now just how monstrous will this be with lambdas (i.e., no subroutine names, except maybe unique-pairs)?
+
+
+; k > i > j
+; this is due to the unfortunate convention used in (prime-sum-pairs)
+(define (unique-triples-with-sum-lambda n sum)
+
+    ; you have to write this shit from the INSIDE OUT to make sense of it...
+        ; assume k is known
+            ; apply "unique-triples-with-k" to (unique-pairs)
+        ; then wrap the whole thing with (lambda (k)) 
+            ; the value of k comes from applying this lambda to something
+            
+    ; writing this way does NOT seem much harder, although it is counter-intuitive
+        ; but READING these things seems like it would be much much harder...
+            
+        
+    (filter                                         ; selects only those triples that add to sum
+        (lambda (seq) 
+            (= sum (accumulate + 0 seq))
+        )
+        
+        (flatmap                                    ; generates "unique-triples" (k i j) with n >= k > i > j >= 1
+            (lambda (k)
+                (map 
+                    (lambda (pair) (cons k pair))
+                    (unique-pairs (- k 1))
+                )
+            )
+            (enumerate-interval 1 n)
+        )
+        
+        
+        ; alternatively, write out (unique-pairs) in lambda form, and cycle indices...
+        ; sadly, i BET this is what the authors intended...
+        ; http://community.schemewiki.org/?sicp-ex-2.41 looks a bit cleaner using one big list instead of appending with cons...
+        
+        ;(flatmap
+        ;    (lambda (i)
+        ;        (flatmap                                ; "map" to loop over j, "flat" to merge all results
+        ;            (lambda (j)
+        ;                (map 
+        ;                    (lambda (k)
+        ;                        (list i j k)
+        ;                    )
+        ;                    (enumerate-interval 1 (- j 1))
+        ;                )
+        ;            )
+        ;            (enumerate-interval 1 (- i 1))
+        ;        )
+        ;    )
+        ;    (enumerate-interval 1 n)
+        ;)
+        
+        ; and here's the same thing with more Scheming indentation, line spacing, and close parens. less ugly but much more confusing to write...
+        ;(flatmap 
+        ;    (lambda (i)
+        ;    (flatmap 
+        ;        (lambda (j)
+        ;            (map    (lambda (k) (list i j k))
+        ;                    (enumerate-interval 1 (- j 1))))
+        ;        (enumerate-interval 1 (- i 1))))
+        ;    (enumerate-interval 1 n))
+    )
 )
 
-; (test-2.41)
+
+
+
+
+
+
+
+(define (test-2.41)
+
+    (define (test n sum) 
+        (newline) (display (unique-triples-with-sum n sum))
+        (newline) (display (unique-triples-with-sum-lambda n sum))
+    )
+    
+    (test 10 21)
+    
+)
+
+(test-2.41)
         
