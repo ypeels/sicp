f90c7602faa379a3d67739143fd465f3069c9879
Jonathan Chen (home) <chen@chinstruments.com>
2014-03-27 22:52:59 -0500

    Exercise 2.57 - my answer, tested. the results aren't that great, but it meets all the customer specifications, so...

--- exercises/2.57-n-sums.scm
+++ exercises/2.57-n-sums.scm
@@ -1,15 +1,37 @@
 (load "2.56-58-symbolic-differentiation.scm")
 
 
-; TODO: simplify all numbers?
-(define (make-sum-2.57 . args)
+                                        ; "the addend of a sum would be the first term [i.e., unchanged], and
+(define (addmul-2.57 expr symbol)       ; the augend would be the sum of the rest of the terms"
+    (let ((end (cddr expr)))                  ; NOT caddr, which would be the FIRST of the rest of the terms. WATCH OUT, boy
+        (if (> (length end) 1)
+            (cons symbol end)           ; here's how you express "sum of the rest of the terms" - just tack the symbol on!
+            (car end)                   ; surprisingly, the rest of deriv is enough to handle summing multiple terms!
+        )                                     ; this is because (deriv (augend exp)) and (deriv multiplicand exp) 
+    )                                         ; recurse down the exp...?
+)
+(define (augend-2.57 expr) 
+    (addmul-2.57 expr '+))
+(define (multiplicand-2.57 expr)
+    (addmul-2.57 expr '*))
 
-    ; no, i think i'm overthinking this... should just be a QUICK PATCH to addend, augend, multiplicand...
-    ;(if (< (length args) 2)
-    ;    (error "too few arguments: make-sum-2.57" (length args))
-    ;    (let 
-
-    
 
 
-;(define 
+
+
+
+(display "\noriginal results, for reference\n")
+(display (deriv '(* (* x y) (+ x 3)) 'x)) (newline)   
+(display (deriv '(* (* x x) x) 'x) ) (newline)
+
+(display "\nnew results using n-argument product/sum\n")
+(define augend augend-2.57) (define multiplicand multiplicand-2.57) 
+(display (deriv '(* x y (+ x 3)) 'x)) (newline)
+(display (deriv '(* x x x) 'x)) (newline)
+
+
+; (+ (* x y) (* y (+ x 3)))
+; (* (+ x x) (* x (+ x x)))
+
+; now, the original code never simplified (+ x x) into (* 2 x), so i'm not gonna bother here either...
+; this exercise just asks for "sums and products of arbitrary numbers of terms"
