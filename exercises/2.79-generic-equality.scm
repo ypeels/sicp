fe0f0f0d606d162df21379318712aaba09e825db
Jonathan Chen (home) <chen@chinstruments.com>
2014-03-31 18:09:10 -0500

    made dead ends more self-evidently dead

--- exercises/2.79-generic-equality.scm
+++ exercises/2.79-generic-equality.scm
@@ -3,38 +3,38 @@
 ; just like the definitions of (add, (sub, ...
 (define (equ? x y) (apply-generic 'equ? x y))
 
-(define (install-equality-2.79)
-
-    (error "ERROR ugh, add directly to the library file 2.77-80, cuz this is just too cheap...")
-
-    (put 'equ? '(scheme-number scheme-number) =)    ; hey lookee, a 1996, maybe 1980's smiley. intentional?
-    
-
-    
-    (define (equal-rationals? r1 r2)                ; r1 and r2 have been stripped of type information when they enter...
-        ;(newline) (display r1) (newline) (display r2)
-    
-        ; now, i COULD modify (install-rational-package, but that's for wusses
-        ; however, this must exploit the fact that (gcd 0 n) = n        
-        
-        ;(equal? (make-rational 0 123) (sub r1 r2))
-        
-        ; no, (apply doesn't work, so you'd need to examine the low-level structure of r1. meh.
-        ;(let ((q1 (apply make-rational r1)) (q2 (apply make-rational r2)))
-        ;    (equal? (make-rational 0 123) (sub q1 q2)))
-        
-        
-        
-        ; ugh, but in the end, if i'm not gonna exploit lower-level routines, i have to use equal?, but why not use that directly??
-        (equal? r1 r2)
-        
-        
-        
-    )
-    (put 'equ? '(rational rational) equal-rationals?)
-    
-    'done
-)
+;;;(define (install-equality-2.79)
+;;;
+;;;    (error "ERROR ugh, add directly to the library file 2.77-80, cuz this is just too cheap...")
+;;;
+;;;    (put 'equ? '(scheme-number scheme-number) =)    ; hey lookee, a 1996, maybe 1980's smiley. intentional?
+;;;    
+;;;
+;;;    
+;;;    (define (equal-rationals? r1 r2)                ; r1 and r2 have been stripped of type information when they enter...
+;;;        ;(newline) (display r1) (newline) (display r2)
+;;;    
+;;;        ; now, i COULD modify (install-rational-package, but that's for wusses
+;;;        ; however, this must exploit the fact that (gcd 0 n) = n        
+;;;        
+;;;        ;(equal? (make-rational 0 123) (sub r1 r2))
+;;;        
+;;;        ; no, (apply doesn't work, so you'd need to examine the low-level structure of r1. meh.
+;;;        ;(let ((q1 (apply make-rational r1)) (q2 (apply make-rational r2)))
+;;;        ;    (equal? (make-rational 0 123) (sub q1 q2)))
+;;;        
+;;;        
+;;;        
+;;;        ; ugh, but in the end, if i'm not gonna exploit lower-level routines, i have to use equal?, but why not use that directly??
+;;;        (equal? r1 r2)
+;;;        
+;;;        
+;;;        
+;;;    )
+;;;    (put 'equ? '(rational rational) equal-rationals?)
+;;;    
+;;;    'done
+;;;)
 
 
 
@@ -45,7 +45,7 @@
     
     
     
-    ; meh, added directly to the "2.77-80" library file to be more in the spirit of the exercise.
+    ; meh, added (equ? code directly to the "2.77-80" library file to be more in the spirit of the exercise.
     
     (define (test x y)
         (display "\n\nx = ") (display x) (display ". equals itself? ") (display (equ? x x))
