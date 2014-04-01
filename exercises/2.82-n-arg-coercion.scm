3534959e55a0cf81782a0d23a8166a9d475095c7
Jonathan Chen (work) <ypeels@live.com>
2014-04-01 13:08:12 -0500

    Exercise 2.82: my vfugly solution works... how do solutions implement n-argument coercion?

--- exercises/2.82-n-arg-coercion.scm
+++ exercises/2.82-n-arg-coercion.scm
@@ -51,29 +51,80 @@
 
 
 
-(define (coercion-n-args-2.82 . args)
+(define (coercion-n-args-2.82 op args)
+
+    (define (coerce-a-to-b a b)
+        (let ((coercer (get-coercion a b)))
+            (cond 
+                ((eq? a b)
+                    (lambda (x) x))                     ; remember exercise 2.81?
+                (coercer 
+                    (lambda (x)
+                        (if (eq? a (type-tag x))
+                            (coercer x)
+                            (error "invalid argument: COERCE-A-TO-B" x a b)
+                        )
+                    )
+                )
+                (else false)                            ; signal that the requested coercion does not exist
+            )
+        )
+    )   
+
 
     (define (coerce x target-type)
-        (if (eq? target-type (type-tag x))
-            x                                           ; no self coercions. Remember Exercise 2.81?
-            ((get-coercion (type-tag x) target-type) x)
+        ((coerce-a-to-b (type-tag x) target-type) x))
+    
+    (define (nlist x n)
+        (if (<= n 0)
+            '()
+            (cons x (nlist x (- n 1)))))
+            
+    (define (can-coerce-list L target-type)             ; could pull in (accumulate, but i'm too lazy
+        (cond
+            ((null? L)
+                true)
+            ((not (coerce-a-to-b (type-tag (car L)) target-type))   ; checks for self-coercion
+                false)
+            (else 
+                (can-coerce-list (cdr L) target-type))
         )
     )
     
+    (define (op-n op type)
+        (get op (nlist type (length args))))
+                
+    
+    
     ; attempt to coerce all the arguments to the type of the first argument, then to the type of the second argument, and so on.
     (define (iter n)
-        (cond 
-            ((>= n (length args))
-                (error "No coerced function available: COERCION-N-ARGS-2.82" args))
-            
-            ; first check that all of args CAN be coerced to nth type 
-            ; then check via (get if a function taking n x nth type exists
-            ; all clear? invoke the function that you (got
-            (else (error "empty stub - i am here"))
-            
-            ; (list-ref args n)
+        (if (>= n (length args))
+            (error "No coerced function available: COERCION-N-ARGS-2.82" args)
+            (let ((nth-type (type-tag (list-ref args n))))
+                (if (or
+                        ; check via if a function taking n x nth type exists
+                        (not (op-n op nth-type))
+                        
+                        ; check that all of args CAN be coerced to nth type 
+                        (not (can-coerce-list args nth-type))
+                    )
+                    
+                    ; conditions not met; move on to next argument
+                    (iter (+ n 1))
+                    
+                    ; all clear! invoke the function on the coerced arguments
+                    (apply 
+                        (op-n op nth-type)
+                        (map 
+                            contents 
+                            (map (lambda (x) (coerce x nth-type)) args)
+                        )
+                    )
+                )
+            )
         )
     )
+    ; this is soooo ugly... sols?
 
         
     (iter 0)
@@ -104,11 +155,13 @@
     
     "\nInstalled foo3, a trivial function that REQUIRES three arguments."
 )
-(define (foo3 a b c) (apply-generic 'foo3 a b c))
+
     
     
 (define (test-2.82)
     (install-3-arg-function)
+    (install-sample-coercion)   ; scheme-number to complex    
+    (define (foo3 a b c) (apply-generic 'foo3 a b c))
     
     (define (test a b c)
         (foo3 a b c)
@@ -122,7 +175,7 @@
         (test x x z)
     )
 )
-(define coercion-n-args coercion-n-args-2.82) (test-2.82)
+; (define coercion-n-args coercion-n-args-2.82) (test-2.82)
         
 
 
