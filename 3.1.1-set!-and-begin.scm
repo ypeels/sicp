a26914caa840bc05004adeeaa8ab5390c8c10cdc
Jonathan Chen (netbook) <ypeels@live.com>
2014-04-03 16:03:11 -0500

    Section 3.1.1 - finally, they're letting me modify variables

---
+++ 3.1.1-set!-and-begin.scm
@@ -0,0 +1,67 @@
+; FINALLY, this is how you have multiple statements in an if clause
+    ; turns out subroutines and cond clauses (and let bodies) have an IMPLICIT begin
+
+; set! is necessarily a special form because
+    ; its arguments are treated asymmetrically
+    ; it does something "non-functional" to its first argument
+
+; is "begin" necessarily a special form? can't you think of it as a procedure that
+    ; evaluates all of its arguments
+    ; returns its last argument
+
+    
+(define deposit
+    (let ((balance 0))              ; interestingly, this initial value binds ONLY ONCE
+        (lambda (amt)
+            (if (> amt 0)
+                (begin
+                    (set! balance (+ balance amt))
+                    balance
+                )
+                "No non-positive deposits! nice try -- DEPOSIT"
+            )
+        )
+    )
+)
+
+
+;(define deposit2
+;    (begin    
+;        (define balance 0)                      ; can't bind name in null syntactic environment
+;        (lambda (amt)                           ; - "compile-time" error - even if uncalled!
+;            (if (> amt 0)
+;                (begin
+;                    (set! balance (+ balance amt))
+;                    balance
+;                )
+;                "No non-positive deposits! nice try -- DEPOSIT2"
+;            )
+;        )
+;    )
+;)
+
+
+(define (deposit-wrong amt) 
+    (let ((balance 0))          ; nope, this REINITIALIZES balance on each invocation of (deposit-wrong
+        (if (> amt 0)
+            (begin
+                (set! balance (+ balance amt))
+                balance
+            )
+            "No non-positive deposits! nice try -- DEPOSIT"
+        )
+    )
+)
+
+(define (test n)
+    (display "\n\ndeposit:  ") (display (deposit n))
+    ;(display "\ndeposit2: ") (display (deposit2 n))
+    (display "\ndeposit-wrong: ") (display (deposit-wrong n))
+)
+
+
+(test 1)
+(test 10)
+(test 0)
+(test -1)
+(test 100)
