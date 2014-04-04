1086c598d208d1d9ab82eac48069499f8a151816
Jonathan Chen (work) <ypeels@live.com>
2014-04-04 17:23:39 -0500

    ah fixed a tiny bug and it works with an internal history, without set! at all, because we only worry about cdr and not branching tree structures

--- exercises/3.18-check-for-cyclic-list.scm
+++ exercises/3.18-check-for-cyclic-list.scm
@@ -4,61 +4,33 @@
 
 ; things are MUCH easier because we only want to know whether successive cdr will cause an infinite loop
     ; we don't WORRY about whether car will lead to lists too
-(define history '())
-
-(define (in-history? pair)
-    (memq pair history)) 
-    
-(define (add-to-history! x)
-    (if (null? history)
-        (set! history (list x)) 
-        (append! history (list x))
-    )
-)
-
 (define (is-cycle? x)
 
-    ; wait, the cyclic list was handled FINE by exercise 3.17... is this just not doable by an internal, iterative history??
-    ;(define (in-history? pair history)
-    ;    ;(newline)(display history)
-    ;    (memq pair history))
+    (define (in-history? pair history)
+        (memq pair history))
 
+    (define (iter L history)    
+        (cond
+            ((or (null? L) (not (pair? L)))
+                false)
+            ((in-history? L history)        ; NOT (car L)!
+                true)  
+            (else
 
-    ;(define (iter L history)
-    ;
-    ;    (cond
-    ;        ((or (null? L) (not (pair? L)))
-    ;            false)
-    ;        ((in-history? L history)        ; NOT (car L)!
-    ;            true)  
-    ;        (else
-    ;            (newline) (display history)
-    ;            (iter 
-    ;                (cdr L)
-    ;                (append history L)
-    ;            )
-    ;        )
-    ;    )
-    ;)
-    ;
-    ;(iter x '())
-    
-    ; adapted from Exercise 3.17
-    (cond
-        ((null? x)     
-            false)
-        ((in-history? x)
-            true)  
-        (else
-            (add-to-history! x)
-            (is-cycle? (cdr x))
+                (iter 
+                    (cdr L)
+                    (append history (list L))   ; (append history L) is NOT right!
+                )
+            )
         )
     )
+    
+    (iter x '())
 )
 
 
 
-;(display (car x)) (display (cadr x)) (display (caddr x))
+
 
 (define (test-3.18)
 
@@ -75,4 +47,4 @@
     (test '(3 4 4))
     (test '(3 (4 5) 548 (3 3 (2))))
 )
-(test-3.18)
+; (test-3.18)
