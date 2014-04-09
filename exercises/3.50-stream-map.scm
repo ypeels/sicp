ed478c3eff02473dd5fe6a0a81f0d362f2f29c01
Jonathan Chen (home) <chen@chinstruments.com>
2014-04-08 21:16:28 -0500

    Section 3.5.1 - introduction to streams, with their whole SET of 'list' operators

---
+++ exercises/3.50-stream-map.scm
@@ -0,0 +1,7 @@
+
+; how about stream-map a**2 + b**2, and filter if integers? this'll pick out right triangles.
+
+; also test against built-in implementation
+
+; is there any way to confirm that it's not running really really far? maybe with the mapped function also calling display?
+; and third, compare with non-stream map
