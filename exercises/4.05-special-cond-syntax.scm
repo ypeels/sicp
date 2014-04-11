274b5f4537bfa0e1f1c03939d660bdeb30eebbbc
Jonathan Chen (work) <ypeels@live.com>
2014-04-11 17:30:41 -0500

    Exercise 4.5 - implementing some special syntax for cond that i didn't even KNOW about

---
+++ exercises/4.05-special-cond-syntax.scm
@@ -0,0 +1,34 @@
+; if nothing else, this chapter REALLY teaches you about Scheme... 
+; great, just what I've always wanted...
+
+
+; i think all you have to do is to change the following
+(define (expand-clauses clauses)
+    ; ...
+            (make-if (cond-predicate first)
+                     (cond-value first);(sequence->exp (cond-actions first))
+                     ; ...
+)
+
+(define (cond-value clause)
+
+    (let (  (predicate (cond-predicate clause)) ; TODO: error-checking
+            (actions (cond-actions clause))
+            )
+    
+        (if (and (pair? actions) (eq? '=> (car actions)))
+        
+            ; the new special case
+            (let ((operator (cadr actions)))
+                (cons operator predicate))      ; sols: (list operator predicate). they're PROBABLY right...
+             
+            ; the original case
+            (sequence->exp actions)
+        )
+    )
+)
+            
+                     
+
+
+
