b0672a25dac2ecc9f7f51ece815128dc7b9561b2
Jonathan Chen (home) <chen@chinstruments.com>
2014-04-13 19:35:24 -0500

    moar planning

--- exercises/4.19-pathological-sequential-definition.scm
+++ exercises/4.19-pathological-sequential-definition.scm
@@ -7,3 +7,6 @@
   
   ; first run this in mit-scheme
   ; then implement let and run in M-Eval
+  
+  ; seems like eva needs to do multiple passes, with potentially nontrivial logic
+    ; what if you had b(a, c), a, c(a)? then you couldn't just read in reverse order
