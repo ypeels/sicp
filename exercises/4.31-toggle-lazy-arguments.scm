fcf9c0fa13dd3e9c1f55b24489d10e9f9564d72c
Jonathan Chen (home) <chen@chinstruments.com>
2014-04-15 22:30:02 -0500

    i think i have a decent plan worked out

--- exercises/4.31-toggle-lazy-arguments.scm
+++ exercises/4.31-toggle-lazy-arguments.scm
@@ -1,2 +1,8 @@
 ; the problem calls this "an upward-compatible extension", i.e., forward-compatible.
 ; but don't they really mean BACKWARDS compatible??
+
+; looks like if you maintain the argument list unmodified, you can "lazily" procrastinate handling it until (apply)
+    ; compound procedure case
+    ; parse the arguments here into a result list, before calling (extend-environment)
+    
+    ; but what about handling memoization? probably just tweak the tags and add an extra conditional to (force-it)
