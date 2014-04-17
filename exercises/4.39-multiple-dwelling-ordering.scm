c336a8682ddc59f202dfd6d54cf5f9160426a7f9
Jonathan Chen (netbook) <ypeels@live.com>
2014-04-17 10:16:19 -0500

    Exercise 4.39 - reordering (require) statements - this dooesn't really help, does it?

--- exercises/4.39-multiple-dwelling-ordering.scm
+++ exercises/4.39-multiple-dwelling-ordering.scm
@@ -1,14 +1,21 @@
-(define (install-multiple-dwelling-4.38)
+(define (install-multiple-dwelling-4.39)
     (ambeval-batch
-
-        ; from 4.3.2
-        ; there's no WAY i was gonna type this in
+    
         '(define (multiple-dwelling)
           (let ((baker (amb 1 2 3 4 5))
                 (cooper (amb 1 2 3 4 5))
                 (fletcher (amb 1 2 3 4 5))
                 (miller (amb 1 2 3 4 5))
                 (smith (amb 1 2 3 4 5)))
+                
+            ; "super" requirements - the most restrictive possible!
+            ; i BET this doesn't really speed up the program anyway.
+            (require (= baker 3))
+            (require (= cooper 2))
+            (require (= fletcher 4))
+            (require (= miller 5))
+            (require (= smith 1))
+                
             (require
              (distinct? (list baker cooper fletcher miller smith)))
             (require (not (= baker 5)))
@@ -16,20 +23,19 @@
             (require (not (= fletcher 5)))
             (require (not (= fletcher 1)))
             (require (> miller cooper))
-            (require (not (= (abs (- smith fletcher)) 1)))         ; disabled for Exercise 4.38
+            (require (not (= (abs (- smith fletcher)) 1)))  
             (require (not (= (abs (- fletcher cooper)) 1)))
             (list (list 'baker baker)
                   (list 'cooper cooper)
                   (list 'fletcher fletcher)
                   (list 'miller miller)
                   (list 'smith smith))))
-                  
+    
         '(define m multiple-dwelling)
     )
 )
 
-
-(define (test-4.38)
+(define (test-4.39)
 
     (load "ch4-ambeval.scm")
     
@@ -39,18 +45,17 @@
     (load "4.38-44-distinct.scm")
     (install-distinct)
 
-    (install-multiple-dwelling-4.38)
+    (install-multiple-dwelling-4.39)
     (ambeval-batch '(define m multiple-dwelling))
     
     (driver-loop)
 )
-;(test-4.38)
+(test-4.39)
 
-; ordered tuples (baker, cooper, fletcher, miller, smith)
-; original answer (3, 2, 4, 5, 1)
-; answers unrestricted by smith/fletcher
-; (1,2,4,3,5)
-; (1,2,4,5,3)
-; (1,4,2,5,3)
-; (3,2,4,5,1) 
-; (3,4,2,5,1)
+; wait, ordering the (require)s only saves you the slight extra overhead of computing extra (require)s.
+; if you reorder statements so that the most restrictive requirements come first, 
+    ; the ONLY savings is not having to run the subsequent (require)s anymore.
+    
+; i BET the method is NEARLY just as slow, even with the "super-requirement" (3, 2, 4, 5, 1)
+
+; reordering would only really come into play if there were only a few cases but thousands of requires.
