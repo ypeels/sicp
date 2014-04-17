d3f5605533b8beffef79d84af05b3e9d8643664f
Jonathan Chen (work) <ypeels@live.com>
2014-04-17 11:10:24 -0500

    Exercise 4.41 - an ordinary Scheme program to solve the multiple dwellings problem. Pretty short and sweet, since it's built on (permutations)

--- exercises/4.41-multiple-dwelling-without-amb.scm
+++ exercises/4.41-multiple-dwelling-without-amb.scm
@@ -10,9 +10,30 @@
                s)))
  
 (define (remove item sequence)
-  (filter (lambda (x) (not (= x item)))
+  (filter (lambda (x) (not (= x item))) ; (equal? x item))) ; can generalize to all sets, not just numerical
           sequence)) 
                
                
-; new code (should be short)               
-; first test (permutations '(1 2 3 4 5))
+; new code. (should be SHORT, aside from the data entry)               
+(define (valid? dwelling-list)
+    (let (  (baker      (list-ref dwelling-list 0))
+            (cooper     (list-ref dwelling-list 1))
+            (fletcher   (list-ref dwelling-list 2))
+            (miller     (list-ref dwelling-list 3))
+            (smith      (list-ref dwelling-list 4))
+            )
+        (and   
+            (not (= baker 5))
+            (not (= cooper 1))
+            (not (= fletcher 5))
+            (not (= fletcher 1))
+            (> miller cooper)
+            (not (= (abs (- smith fletcher)) 1))    ; can comment out to reproduce 4.38, if desired
+            (not (= (abs (- fletcher cooper)) 1))
+        )
+    )
+)
+
+; (permutations) automatically filters for (distinct?)
+(display (filter valid? (permutations '(1 2 3 4 5))))
+; ((3 2 4 5 1))
