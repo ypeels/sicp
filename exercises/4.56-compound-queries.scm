3bdc0cd715e6ac0131547e6ea6f2837c1dd31320
Jonathan Chen (netbook) <ypeels@live.com>
2014-04-18 17:10:52 -0500

    Exercise 4.56 - compound queries. so something cool sounding like 'logic programming' turns out to be as pedestrian as a database query language...

---
+++ exercises/4.56-compound-queries.scm
@@ -0,0 +1,24 @@
+(display "\na. (and (supervisor ?slave (Bitdiddle Ben)) (address ?slave ?where))")
+;(and (supervisor (tweakit lem e) (bitdiddle ben)) (address (tweakit lem e) (boston (bay state road) 22)))
+;(and (supervisor (fect cy d) (bitdiddle ben)) (address (fect cy d) (cambridge (ames street) 3)))
+;(and (supervisor (hacker alyssa p) (bitdiddle ben)) (address (hacker alyssa p) (cambridge (mass ave) 78)))
+
+(display "\nb. 
+(and (salary (Bitdiddle Ben) ?ben-salary)
+     (salary ?master ?master-salary)
+     (lisp-value > ?master-salary ?ben-salary))
+")
+;(and (salary (bitdiddle ben) 60000) (salary (scrooge eben) 75000) (lisp-value > 75000 60000))
+;(and (salary (bitdiddle ben) 60000) (salary (warbucks oliver) 150000) (lisp-value > 150000 60000))
+
+(display "\nc.
+(and (supervisor ?slave ?master)
+     (not (job ?master (computer . ?type)))
+     (job ?master ?master-job))")   
+;(and (supervisor (aull dewitt) (warbucks oliver)) (not (job (warbucks oliver) (computer . ?type))) (job (warbucks oliver) (administration big wheel)))
+;(and (supervisor (cratchet robert) (scrooge eben)) (not (job (scrooge eben) (computer . ?type))) (job (scrooge eben) (accounting chief accountant)))
+;(and (supervisor (scrooge eben) (warbucks oliver)) (not (job (warbucks oliver) (computer . ?type))) (job (warbucks oliver) (administration big wheel)))
+;(and (supervisor (bitdiddle ben) (warbucks oliver)) (not (job (warbucks oliver) (computer . ?type))) (job (warbucks oliver) (administration big wheel)))
+
+(load "ch4-query.scm")
+(run)
