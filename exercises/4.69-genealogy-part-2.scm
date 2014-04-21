a76c6d1c827556155c5cf524da658f69e5319cb7
Jonathan Chen (work) <ypeels@live.com>
2014-04-21 13:00:22 -0500

    still getting nowhere

--- exercises/4.69-genealogy-part-2.scm
+++ exercises/4.69-genealogy-part-2.scm
@@ -6,15 +6,41 @@
 
     ; Write rules that determine if a list ends in the word grandson.
     (query '(assert! (rule (ends-with-grandson (?L)) (last-pair (?L) grandson))))
-        ; shouldn't ONE rule suffice? 
+        ; shouldn't ONE rule suffice? oh i guess last-pair is a rule in itself too...
         ; maybe allow for singleton too
-        (query '(assert! (rule (ends-with-grandson grandson))))
+        ;(query '(assert! (rule (ends-with-grandson grandson))))
+    
+        ; test it
+        ;(query '(assert! ((great grandson) Lupin1 Lupin4)))
+        (query '(assert! (rule (findgs ?p1 ?p2)
+            (and
+                (?relationship ?p1 ?p2)
+                (ends-with-grandson ?relationship)
+            )
+        )))
+        (query '(findgs ?p1 ?p2))
+    
+    ; works for a single great... but that's NOT what we want...
+    ;(query '(assert! (rule ((great grandson) ?x ?y) (and (same ?rel (grandson)) (grandson ?x ?gs) (son ?gs ?y)))))
+    
     
     ; Use this to express a rule that allows one to derive the relationship ((great . ?rel) ?x ?y), 
     ; where ?rel is a list ending in grandson.)
-    (query '(assert! (rule ((great . ?rel)
+    (query '(assert! (rule ((great . ?rel) ?x ?y)
     
-    ; don't forget the query (grandson ?S ?G)
+        (and
+            (ends-with-grandson (?rel))
+            (or
+                (and (grandson ?x ?gs) (son ?gs ?y))
+                
+                ; no WAY this is gonna work...?
+                ;(?rel (son ?x) ?y)
+            )
+        )
+    )))
+    
+    
+    ; don't forget the query (grandson ?G ?S)
 )
 
 (define (test-4.69)
@@ -27,6 +53,11 @@
     
     (load "4.62-last-pair-via-logic.scm")
     (install-last-pair) ; for ends-in-grandson
+    
+    (install-great-genealogy)
+    
+    ; test cases from problem statement
+    ;(query '((great grandson) ?g ?ggs))
     
     (query-driver-loop)
 )
