1fae22f871cca40f7c936f9be973e2bcc2b9e5fd
Jonathan Chen (work) <ypeels@live.com>
2014-04-23 13:06:19 -0500

    doesn't crash, and ALMOST returns a good answer...

--- exercises/4.76-refactoring-and.scm
+++ exercises/4.76-refactoring-and.scm
@@ -17,9 +17,82 @@
 
     ; iterate over the list of streams. each stream is a stream of frames.
         ; if there is only 1 stream left, the merging is done! return.
-        ; for the first 2 streams, loop over all entries in BOTH (nested stream-flatmap or something?)
-            ; accumulate any successful merges of frames
-    ;(define (iter 
+    (define (merge-all stream-list)
+    
+        ;(display "\nmerge-all") (display stream-list)
+        (cond
+            ((null? stream-list)
+                (error "Null list -- MERGE-ALL"))
+            ((null? (cdr stream-list))
+                (car stream-list))
+            (else
+                (let (  (first-stream (car stream-list))
+                        (second-stream (cadr stream-list))
+                        (rest-streams (cddr stream-list))
+                        )
+            
+                    (merge-all 
+                        (cons 
+                            (merge-2-streams first-stream second-stream)
+                            rest-streams
+                        )
+                    )
+                )
+            )
+        )
+    )
+    
+    ; for 2 streams, loop over all entries in BOTH (nested stream-flatmap or something?)
+        ; accumulate any successful merges of frames
+    (define (merge-2-streams stream1 stream2)
+
+        ;(display "\nmerge-2-streams") (display stream1) (display stream2)
+        
+        (let ((result 
+        
+                ; loop over stream1
+                (stream-flatmap        
+                    (lambda (frame1)   
+
+                        (let ((result1 
+                    
+                    
+                                ; loop over stream 2 (so awkwardly/impenetrably)
+                                (stream-map ;flatmap ; oh oops, flatmap CRASHES this?
+                                    (lambda (frame2)    
+                                    
+                                        (let ((merge-result (merge-frames-if-possible frame1 frame2)))
+                                            ;(display "\nmerge-2-streams: inner result ") (display merge-result)
+                                            (if (eq? merge-result 'failed-merge)
+                                                '() ; i THINK stream-map will automagically filter out null entries??
+                                                merge-result
+                                            )
+                                        )     
+                                        
+                                    )
+                                    stream2
+                                )
+                            
+                                ))
+                                
+                            ;(display "middle result") (display result1) 
+                            result1
+                        )
+                    
+                    )
+                    stream1
+                )
+            ))
+            
+            ;(display "\nmerge-2-streams done") (display result)
+            result
+        )
+            
+    )
+                    
+                    
+                    
+        
     
         
 
@@ -27,6 +100,9 @@
     
     (let ((evaluated-conjuncts (map (lambda (c) (qeval c frame-stream)) conjuncts)))
         ; each member of the list evaluated-conjuncts is a stream of frames.
+        
+        (merge-all evaluated-conjuncts)
+    )
 )
 
 
@@ -39,7 +115,8 @@
         ; need to signal error if (extend-if-possible) failed...
 ; how do i loop over a frame?
     ; from (extend), a frame is just a list of (var . value) pairs.
-(define (merge-if-possible source-frame dest-frame)
+(define (merge-frames-if-possible source-frame dest-frame)
+    ;(display "\nmerge-if-possible") (display dest-frame)
     (cond 
         ((eq? dest-frame 'failed) 'failed-merge)
         ((null? source-frame) dest-frame)
@@ -47,7 +124,7 @@
             (let (  (first-binding (car source-frame))
                     (rest-bindings (cdr source-frame))   
                     )
-                (merge-if-possible
+                (merge-frames-if-possible
                     rest-bindings
                     (extend-if-possible ; var val frame
                         (binding-variable first-binding)
