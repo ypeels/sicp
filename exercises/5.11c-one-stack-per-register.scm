3b0a6672392c14c0b9098704da2b844157d80cdd
Jonathan Chen (work) <ypeels@live.com>
2014-04-28 15:20:48 -0500

    groundwork

---
+++ exercises/5.11c-one-stack-per-register.scm
@@ -0,0 +1,7 @@
+; Extreme Overkill
+; c.  (restore y) puts into y the last value saved from y regardless of what other registers were saved after 
+;       y and not restored. Modify the simulator to behave this way. You will have to associate a separate 
+;       stack with each register. You should make the initialize-stack operation initialize all the register stacks. 
+
+
+; starting point: (initialize-stack). when is this ever called??
