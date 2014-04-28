7284f7593e37cb75149e992e714f6e2e831a3b1b
Jonathan Chen (work) <ypeels@live.com>
2014-04-28 17:30:36 -0500

    basic infrastructure, and ready to write code for the easiest case

--- exercises/5.12-data-listing-from-assembly.scm
+++ exercises/5.12-data-listing-from-assembly.scm
@@ -30,5 +30,76 @@
     ; a is actually similar to d, except that the table rows are KNOWN from p. 513.
     
     ; could create a gigantic merged 2-d table, but there MIGHT be name collisions from register names
-    ; should probably create 2 1-d tables and 2 2-d tables (start by testing the former)
+    ; should probably create 3 1-d tables and 2 2-d tables (start by testing the former)
     
+
+    
+; new data structures for logging
+; new interface function to dump logs
+(define (make-new-machine-5.12)                                             
+  (let ((pc (make-register 'pc))                                       
+        (flag (make-register 'flag))                                   
+        (stack (make-stack))
+        (the-instruction-sequence '())
+        (entry-datalist '())                                            ; <---- new data structures
+       )                                
+    (let ((the-ops                                                     
+           (list (list 'initialize-stack
+                       (lambda () (stack 'initialize)))                
+                 ;;**next for monitored stack (as in section 5.2.4)
+                 ;;  -- comment out if not wanted
+                 (list 'print-stack-statistics                         
+                       (lambda () (stack 'print-statistics)))))
+          (register-table                                              
+           (list (list 'pc pc) (list 'flag flag))))                    
+      (define (allocate-register name)                                 
+        (if (assoc name register-table)                                
+            (error "Multiply defined register: " name)
+            (set! register-table
+                  (cons (list name (make-register name))
+                        register-table)))
+        'register-allocated)
+      (define (lookup-register name)                                   
+        (let ((val (assoc name register-table)))                       
+          (if val                                                      
+              (cadr val)
+              (error "Unknown register:" name))))
+      (define (execute)
+        (let ((insts (get-contents pc)))                               
+          (if (null? insts)
+              'done
+              (begin                                                   
+                ((instruction-execution-proc (car insts)))             
+                (execute)))))        
+      (define (print-datalists)                                         ; <---- new procedure
+        (display "\nEntry points\n")
+        (display entry-datalist)
+      )
+      (define (dispatch message)
+        (cond ((eq? message 'start)                                    
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)             
+               (lambda (seq) (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register) allocate-register)     
+              ((eq? message 'get-register) lookup-register)            
+              ((eq? message 'install-operations)                       
+               (lambda (ops) (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)                             
+              ((eq? message 'operations) the-ops)                      
+              ((eq? message 'print-datalists) (print-datalists))        ; <---- new message "to provide access to the new information"
+              (else (error "Unknown request -- MACHINE" message))))
+      dispatch)))
+      
+; TODO: modify (make-goto)
+      
+      
+; -------------------------------------------------------------------------
+      
+      
+(load "ch5-regsim.scm")
+(load "5.06-fibonacci-extra-push-pop.scm")
+(define make-new-machine make-new-machine-5.12)
+
+(define fib-machine (make-new-machine))
+(fib-machine 'print-datalists)
