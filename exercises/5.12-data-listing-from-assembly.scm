8ba3813f7d2eb13af4dd9a9caed2748777489d7e
Jonathan Chen (home) <chen@chinstruments.com>
2014-04-28 20:54:10 -0500

    assign-logging works too, but i was too lazy with my data structures, and logging all instructions will kill my instruction list...)

--- exercises/5.12-data-listing-from-assembly.scm
+++ exercises/5.12-data-listing-from-assembly.scm
@@ -36,15 +36,45 @@
     
 ; new data structures for logging
 ; new interface function to dump logs
-(define (make-new-machine-5.12)                                             
+(define (make-new-machine-5.12)
+    (define (make-empty-datalist sym)                                   ; new helper function
+        (list                                                               ; not init to '() because of (append!) quirk...?
+            (string->symbol                                                 ; would set! + cons work better? meh
+                (string-append
+                    "*"
+                    (symbol->string sym)
+                    "-datalist*"
+                )
+            )
+        )
+    )
+
+                                            
   (let ((pc (make-register 'pc))                                       
         (flag (make-register 'flag))                                   
         (stack (make-stack))
         (the-instruction-sequence '())
-        (entry-datalist '(*entry-datalist*))                            ; <---- new data structures
-        (save-datalist '(*save-datalist*))                              ; not init to empty because of (append!) quirk...
-        (restore-datalist '(*restore-datalist*))
-       )                                
+        (entry-datalist (make-empty-datalist 'entry))                            ; <---- new data structures
+        (save-datalist (make-empty-datalist 'save))        
+        (restore-datalist (make-empty-datalist 'restore))
+        (instruction-datalist-table 
+            (list
+                (cons 'assign (make-empty-datalist 'assign))
+                (cons 'branch (make-empty-datalist 'branch))
+                (cons 'goto (make-empty-datalist 'goto))
+                (cons 'perform (make-empty-datalist 'perform))
+                (cons 'restore (make-empty-datalist 'restore))
+                (cons 'save (make-empty-datalist 'save))
+                (cons 'test (make-empty-datalist 'test))
+            )                
+        )
+        (assign-datalist-table                                                  ; could also put in the next let... but this keeps all datalists together
+            (list
+                (cons 'pc (make-empty-datalist 'pc))
+                (cons 'flag (make-empty-datalist 'flag))                        
+            )
+        )
+       )
     (let ((the-ops                                                     
            (list (list 'initialize-stack
                        (lambda () (stack 'initialize)))                
@@ -57,9 +87,16 @@
       (define (allocate-register name)                                 
         (if (assoc name register-table)                                
             (error "Multiply defined register: " name)
-            (set! register-table
-                  (cons (list name (make-register name))
-                        register-table)))
+            (begin
+                (set! assign-datalist-table                                     ; <---- new: add new register to register-datalist-table
+                      (cons
+                        (list name (make-empty-datalist name))
+                        assign-datalist-table))
+                (set! register-table
+                      (cons (list name (make-register name))
+                            register-table));)
+            )
+        )
         'register-allocated)
       (define (lookup-register name)                                   
         (let ((val (assoc name register-table)))                       
@@ -74,13 +111,42 @@
                 ((instruction-execution-proc (car insts)))             
                 (execute)))))        
       (define (print-datalists)                                         ; <---- new procedures
-        (display "\n\nRegisters used by (goto)\n")
-        (display (cdr entry-datalist))
-        (display "\n\nRegisters used by (save)\n")
-        (display (cdr save-datalist))
-        (display "\n\nRegisters used by (restore)\n")
-        (display (cdr restore-datalist))
+        (define (print-single-datalist datalist title)
+            (newline)
+            (display title)
+            (newline)
+            (display (cdr datalist))
+            (newline)
+        )
+        (define (print-datalist-table table title)
+            (for-each 
+                (lambda (datalist) 
+                    (print-single-datalist datalist title))
+                table
+            )
+        )
+        
+        (print-single-datalist entry-datalist "Registers used by (goto)")
+        (print-single-datalist save-datalist "Registers used by (save)")
+        (print-single-datalist restore-datalist "Registers used by (restore)")
+        (print-datalist-table assign-datalist-table "Assignments")
+        ;(display assign-datalist-table)
+        ;(print-datalist-table instruction-datalist-table "Instructions")
+        (for-each
+            (lambda (datalist) (newline) (display datalist))
+            instruction-datalist-table) ; otherwise it's just too cluttered
+            
+      
+        ;(display "\n\nRegisters used by (goto)\n")
+        ;(display (cdr entry-datalist))
+        ;(display "\n\nRegisters used by (save)\n")
+        ;(display (cdr save-datalist))
+        ;(display "\n\nRegisters used by (restore)\n")
+        ;(display (cdr restore-datalist))
+        ;(display "\n\nRegister data sources\n")
       )
+      
+      ; single-datalist functions
       (define (add-to-entry-datalist! register-name)
         (add-to-datalist! register-name entry-datalist))
       (define (add-to-save-datalist! register-name)
@@ -91,15 +157,41 @@
         (cond
             ((symbol? datum) (memq datum (cdr datalist)))               ; (append!) doesn't like empty lists?!
             ((list? datum) (member datum (cdr datalist)))
-            (else (error "Unknown data type -- IS-IN-DATALIST?" entry))
+            (else (error "Unknown data type -- IS-IN-DATALIST?" datum))
         )
       )
-      (define (add-to-datalist! entry datalist)
-        (if (not (is-in-datalist? entry datalist))
-            (append! datalist (list entry))
+      (define (add-to-datalist! datum datalist)
+        (if (not (is-in-datalist? datum datalist))
+            (append! datalist (list datum))
         )
         'done
       )            
+      
+      ; datalist table functions. figuring out my data structures was 90% of the battle
+      (define (get-datalist-from-table name table)
+        (let ((datalist (assoc name table)))
+            (if datalist
+                datalist
+                (error "Datalist not found -- GET-DATALIST-FROM-TABLE" name table)
+            )
+        )
+      )
+      
+      (define (add-to-assign-datalists! register-name value-exp)
+        (let ((datalist (get-datalist-from-table register-name assign-datalist-table)))
+            (add-to-datalist! value-exp datalist)
+        )
+      )      
+      (define (add-to-instruction-datalists! instruction-type expr)
+        ;(display instruction-type)(newline)
+        ;(display expr)(newline)(newline)
+        (let ((datalist (get-datalist-from-table instruction-type instruction-datalist-table)))
+            (add-to-datalist! instruction-type expr)
+        )
+      )      
+      
+      
+      
       (define (dispatch message)
         (cond ((eq? message 'start)                                    
                (set-contents! pc the-instruction-sequence)
@@ -117,6 +209,8 @@
               ((eq? message 'log-entry) add-to-entry-datalist!)
               ((eq? message 'log-save) add-to-save-datalist!)
               ((eq? message 'log-restore) add-to-restore-datalist!)
+              ((eq? message 'log-assign) add-to-assign-datalists!)
+              ((eq? message 'log-instruction) add-to-instruction-datalists!)
               
               (else (error "Unknown request -- MACHINE" message))))
       dispatch)))
@@ -155,8 +249,44 @@
     (lambda ()
       (set-contents! reg (pop stack))    
       (advance-pc pc))))
-
       
+; modified for case d
+(define (make-assign-5.12 inst machine labels operations pc)   
+  ((machine 'log-assign) (assign-reg-name inst) (assign-value-exp inst))
+  (let ((target
+         (get-register machine (assign-reg-name inst)))             
+        (value-exp (assign-value-exp inst)))                        
+    (let ((value-proc                                               
+           (if (operation-exp? value-exp)
+               (make-operation-exp                                  
+                value-exp machine labels operations)
+               (make-primitive-exp                                  
+                (car value-exp) machine labels))))
+      (lambda ()                ; execution procedure for assign    
+        (set-contents! target (value-proc))                         
+        (advance-pc pc)))))
+        
+; modified for case a
+(define (make-execution-procedure-5.12 inst labels machine         
+                                  pc flag stack ops)  
+  ;((machine 'log-instruction) (car inst) (cdr inst))                    ; <---- new logging code (1 line)
+  ;(display (car inst)) (newline)
+  (cond ((eq? (car inst) 'assign)                             
+         (make-assign inst machine labels ops pc))
+        ((eq? (car inst) 'test)
+         (make-test inst machine labels ops flag pc))         
+        ((eq? (car inst) 'branch)                             
+         (make-branch inst machine labels flag pc))
+        ((eq? (car inst) 'goto)                               
+         (make-goto inst machine labels pc))
+        ((eq? (car inst) 'save)
+         (make-save inst machine stack pc))
+        ((eq? (car inst) 'restore)
+         (make-restore inst machine stack pc))
+        ((eq? (car inst) 'perform)
+         (make-perform inst machine labels ops pc))
+        (else (error "Unknown instruction type -- ASSEMBLE"
+                     inst))))      
       
 ; -------------------------------------------------------------------------
       
@@ -169,6 +299,14 @@
 (define make-goto make-goto-5.12)
 (define make-save make-save-5.12)
 (define make-restore make-restore-5.12)
+(define make-assign make-assign-5.12)
+(define make-execution-procedure make-execution-procedure-5.12)
 
 (define fib-machine (make-fib-machine-5.6))
+
+; regression test
+(test-5.6-long)
+
+; test new functionality
 (fib-machine 'print-datalists)
+
