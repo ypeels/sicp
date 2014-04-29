dc2b09c12286c28fa5b827c99966c65c99268823
Jonathan Chen (work) <ypeels@live.com>
2014-04-29 13:39:38 -0500

    spacing

--- exercises/5.13-dynamic-register-allocation.scm
+++ exercises/5.13-dynamic-register-allocation.scm
@@ -6,35 +6,37 @@
             (register-list '(pc flag))
             )
     
-    (define (allocate-register-5.13 name)                               
-      ;(display "\nnot allocating ")
-      ;(display name)
-      'empty-stub) ; doesn't get passed to base class's allocate-register.
-    
-    ; actually, just hijack the base class's register table!    
-    ; maintain a local list of allocated registers, to avoid (lookup)'s (error).
-    (define (lookup-register-5.13 name)                                 
-        (if (memq name register-list)
-            ((machine 'get-register) name)
-            (begin
-                ;(display "\nAllocating register: ") (display name)
-                (set! register-list (cons name register-list))
-                ((machine 'allocate-register) name)                     ; inefficient, but should work             
+        (define (allocate-register-5.13 name)                               
+          ;(display "\nnot allocating ")
+          ;(display name)
+          'empty-stub) ; doesn't get passed to base class's allocate-register.
+        
+        ; actually, just hijack the base class's register table!    
+        ; maintain a local list of allocated registers, to avoid (lookup)'s (error).
+        (define (lookup-register-5.13 name)                                 
+            (if (memq name register-list)
                 ((machine 'get-register) name)
+                (begin
+                    ;(display "\nAllocating register: ") (display name)
+                    (set! register-list (cons name register-list))
+                    ((machine 'allocate-register) name)                     ; inefficient, but should work             
+                    ((machine 'get-register) name)
+                )
             )
         )
-    )
-    ; that should do 'er!
+        ; that should do 'er!
 
-    (define (dispatch message)
-      (cond 
-            ; overrides
-            ((eq? message 'allocate-register) allocate-register-5.13)   
-            ((eq? message 'get-register) lookup-register-5.13)  
-            
-            ; base class
-            (else (machine message))))
-    dispatch))
+        (define (dispatch message)
+          (cond 
+                ; overrides
+                ((eq? message 'allocate-register) allocate-register-5.13)   
+                ((eq? message 'get-register) lookup-register-5.13)  
+                
+                ; base class
+                (else (machine message))))
+        dispatch
+    )
+)
     
 (load "ch5-regsim.scm")
 (load "5.06-fibonacci-extra-push-pop.scm")
