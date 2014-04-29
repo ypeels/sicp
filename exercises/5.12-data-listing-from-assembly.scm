6e2deb5a94767c23ef7213d5c3c7d147c697a74c
Jonathan Chen (work) <ypeels@live.com>
2014-04-29 12:01:52 -0500

    explicit version for wiki http://community.schemewiki.org/?sicp-ex-5.12

--- exercises/5.12-data-listing-from-assembly.scm
+++ exercises/5.12-data-listing-from-assembly.scm
@@ -57,7 +57,7 @@
 
         (define (is-in-dataset? datum)
             (cond
-                ((symbol? datum) (memq datum (cdr dataset)))   ; first entry is a dummy header
+                ((symbol? datum) (memq datum (cdr dataset)))   ; skip dummy header
                 ((list? datum) (member datum (cdr dataset)))
                 (else (error "Unknown data type -- IS-IN-dataset?" datum))))        
             
@@ -75,6 +75,13 @@
     (dataset 'print))
     
 
+    
+; the rest of this solution "overrides" existing functions in ch5-regsim.scm
+    ; that is, they add a bit of functionality, then call the existing functions.
+    ; this way, readers (the author included) don't have to compare code
+        ; just to figure out what was added/changed.
+        
+    
 
 ; implemented as a "facade" in front of the old machine
 (define (make-new-machine-5.12)             
@@ -151,6 +158,7 @@
         ; punt everything else to "base class" / delegate - INCLUDING error handling
         (else (machine-regsim message))))                         
     dispatch))   
+    
 
     
 (define (make-execution-procedure-5.12 inst labels machine pc flag stack ops)  
@@ -185,9 +193,9 @@
       
 ; -------------------------------------------------------------------------
       
+; example usage.
       
 (load "ch5-regsim.scm")
-(load "5.06-fibonacci-extra-push-pop.scm")
 
 ; make the overrides official.
 (define make-new-machine-regsim make-new-machine) 
@@ -208,13 +216,48 @@
 (define make-execution-procedure-regsim make-execution-procedure) 
 (define make-execution-procedure make-execution-procedure-5.12)
 
-(define fib-machine (make-fib-machine-5.6))
 
-; regression test
-(test-5.6-long)
+(define fib-machine (make-machine ;register-names ops controller-text
+    '(n val continue)
+    (list (list '< <) (list '- -) (list '+ +))
+    '(  ; from ch5.scm
+           (assign continue (label fib-done))
+         fib-loop
+           (test (op <) (reg n) (const 2))
+           (branch (label immediate-answer))
+           ;; set up to compute Fib(n-1)
+           (save continue)
+           (assign continue (label afterfib-n-1))
+           (save n)                           ; save old value of n
+           (assign n (op -) (reg n) (const 1)); clobber n to n-1
+           (goto (label fib-loop))            ; perform recursive call
+         afterfib-n-1                         ; upon return, val contains Fib(n-1)
+           (restore n)
+           (restore continue)
+           ;; set up to compute Fib(n-2)
+           (assign n (op -) (reg n) (const 2))
+           (save continue)
+           (assign continue (label afterfib-n-2))
+           (save val)                         ; save Fib(n-1)
+           (goto (label fib-loop))
+         afterfib-n-2                         ; upon return, val contains Fib(n-2)
+           (assign n (reg val))               ; n now contains Fib(n-2)
+           (restore val)                      ; val now contains Fib(n-1)
+           (restore continue)
+           (assign val                        ; Fib(n-1)+Fib(n-2)
+                   (op +) (reg val) (reg n)) 
+           (goto (reg continue))              ; return to caller, answer is in val
+         immediate-answer
+           (assign val (reg n))               ; base case: Fib(n)=n
+           (goto (reg continue))
+         fib-done)))
 
-; test new functionality
-(fib-machine 'print-all-datasets)
+
+
+;(load "5.06-fibonacci-extra-push-pop.scm")
+;(define fib-machine (make-fib-machine-5.6))
+;(test-5.6-long) ; regression test
+(fib-machine 'print-all-datasets) ; test new functionality
 
 ; ;;; Results - cf. p. 497
 ; Registers used by goto
