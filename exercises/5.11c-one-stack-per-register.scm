f0e9c90c5f85bbb6c54d125052791169a0faf345
Jonathan Chen (work) <ypeels@live.com>
2014-04-29 14:05:55 -0500

    converted to facade implementation, so you don't have to look at COMMENTS to figure out which part of the solution is new

--- exercises/5.11c-one-stack-per-register.scm
+++ exercises/5.11c-one-stack-per-register.scm
@@ -32,121 +32,68 @@
     
 
 ; modified from ch5-regsim.scm - changes have been annotated on the right.
+; TODO: fix initialize-stack and print-stack-statistics API, which is currently broken
+    ; should print out/initialize ALL stacks in the table.
+    ; would probably have to modify the base class... meh
 (define (make-new-machine-5.11c)
-  (let ((pc (make-register 'pc))                                         
-        (flag (make-register 'flag))                                     
-        ;(stack (make-stack))                                               ; <---- commented out.
-        (the-instruction-sequence '()))                                  
-    (let ((the-ops                                                       
-           (list ;(list 'initialize-stack                                   ; hmm, this public api breaks...
-                 ;      (lambda () (stack 'initialize)))                  
-                 ;;**next for monitored stack (as in section 5.2.4)
-                 ;;  -- comment out if not wanted
-                 ;(list 'print-stack-statistics                           
-                 ;      (lambda () (stack 'print-statistics)))))
-                 ))
-          (stack-table                                                      ; <----- new, by analogy with register-table. (user COULD in principle push/pop these!!)                       
-           (list (list 'pc (make-stack)) (list 'flag (make-stack))))        
-          (register-table                                                
-           (list (list 'pc pc) (list 'flag flag))))                         ; alternative: (list 'pc pc (make-stack)). BUT then more dependent code might change...?
-      (define (allocate-register name)                                      
-        (if (assoc name register-table)                                  
-            (error "Multiply defined register: " name)
-            (begin                                                          
-                (set! stack-table                                           ; <----- new, by analogy with register-table. why bother thinking??
-                    (cons (list name (make-stack)) stack-table))                ; why modify (initialize-stack) when this is error-prone??
-                (set! register-table
-                      (cons (list name (make-register name))
-                            register-table));)
-            )
-        )
-        'register-allocated)
-      (define (lookup-register name)                                     
-        (let ((val (assoc name register-table)))                         
-          (if val                                                        
-              (cadr val)
-              (error "Unknown register:" name))))
+    (let (  (machine (make-new-machine-regsim)) 
+            (stack-table                                                      ; <----- new, by analogy with register-table. (user COULD in principle push/pop these!!)                       
+                (list (list 'pc (make-stack)) (list 'flag (make-stack)))))
+                                  
+           
+           
+      ; overrides
+      (define (allocate-register-5.11c name)
+        (set! stack-table                                          
+            (cons (list name (make-stack)) stack-table))
+        ((machine 'allocate-register) name))                                ; defer to base class error-checking
+              
+              
       (define (lookup-stack name)                                           ; <----- new, by analogy with lookup-register                         
         (let ((val (assoc name stack-table)))                         
           (if val                                                        
               (cadr val)
               (error "Unknown stack:" name))))
-      (define (execute)
-        (let ((insts (get-contents pc)))                                 
-          (if (null? insts)
-              'done
-              (begin                                                     
-                ((instruction-execution-proc (car insts)))               
-                (execute)))))                                            
+              
+                                         
       (define (dispatch message)
-        (cond ((eq? message 'start)                                      
-               (set-contents! pc the-instruction-sequence)
-               (execute))
-              ((eq? message 'install-instruction-sequence)               
-               (lambda (seq) (set! the-instruction-sequence seq)))
-              ((eq? message 'allocate-register) allocate-register)       
-              ((eq? message 'get-register) lookup-register)              
-              ((eq? message 'install-operations)                         
-               (lambda (ops) (set! the-ops (append the-ops ops))))
-              
-              ; full breakdown
-              ; (machine 'stack) is ONLY called from (update-insts!)
-              ; the stack object is passed as an object to (make-execution-procedure)
-              ; but ONLY (make-save) and (make-restore) actually use it further, and i CONTROL these!
-              ;((eq? message 'stack) stack)                                 
-              ((eq? message 'stack) 'unused-return-value)
-              
-              ((eq? message 'get-stack) lookup-stack)                       ; <----- new, by analogy with get-register
-              
-              ((eq? message 'operations) the-ops)                        
-              (else (error "Unknown request -- MACHINE" message))))
-      dispatch)))    
+        (cond
+        
+            ; overrides
+            ((eq? message 'allocate-register) allocate-register-5.11c)
+                
+
+        
+            ; full breakdown
+            ; (machine 'stack) is ONLY called from (update-insts!)
+            ; the stack object is passed as an object to (make-execution-procedure)
+            ; but ONLY (make-save) and (make-restore) actually use it further, and i CONTROL these!
+            ;((eq? message 'stack) stack)  
+            ((eq? message 'stack) 'unused-return-value)
+            
+            
+            ; new
+            ((eq? message 'get-stack) lookup-stack)
+            
+            ; punt
+            (else (machine message))))
+
+      dispatch))    
       
     
-    
-; ok, so (push) and (pop) are ONLY called inside (save) and (restore).
-    ; can keep the syntax from part b: (push (<name> . <value>))
-    ; but the (push) and (pop) convenience functions are going to have to do something different.
     
 ; by analogy with (get-register)
 (define (get-stack machine stack-name)
     ((machine 'get-stack) stack-name))
     
-(define (make-save-5.11c inst machine unused-argument pc)   ; kept unused-argument to minimize changes to existing code                         
-  ;(let ((reg (get-register machine
-  ;                         (stack-inst-reg-name inst)));)
-  (let ((name (stack-inst-reg-name inst)))
-    (let (  (reg (get-register machine name))
-            (stack (get-stack machine name))
-            )
-  
-        (lambda ()        
-          (push stack (get-contents reg))         ;(push stack reg) ; no need to modify this, though!!          
-          (advance-pc pc));))                                             
-    )
-  )
-  
-  ;(make-save-regsim
-  ;  inst
-  ;  machine
-  ;  (get-stack machine (stack-inst-reg-name inst))
-  ;  pc
-  ;)  
-)
+(define (make-save-5.11c inst machine unused-argument pc) 
+    (let ((stack (get-stack machine (stack-inst-reg-name inst))))
+        (make-save-regsim inst machine stack pc)))
 
 (define (make-restore-5.11c inst machine unused-argument pc)                         
-  ;(let ((reg (get-register machine
-  ;                         (stack-inst-reg-name inst)));)
-  (let ((name (stack-inst-reg-name inst)))
-    (let (  (reg (get-register machine name))
-            (stack (get-stack machine name))
-            )
-        (lambda ()    
-          (set-contents! reg (pop stack))      ; (pop! stack reg) ; modifies stack AND reg. ah, that's a nice and pleasing symmetry      
-          (advance-pc pc));))
-    )
-  )
-)
+    (let ((stack (get-stack machine (stack-inst-reg-name inst))))
+        (make-restore-regsim inst machine stack pc))) 
+
     
 
 
@@ -191,6 +138,7 @@
     (set-register-contents! recursive-expt-machine 'b 2)
     (set-register-contents! recursive-expt-machine 'n 5)
     (start recursive-expt-machine)
+    (display "\nAnd what is 2**5? ")
     (display (get-register-contents recursive-expt-machine 'val)) ; 32, right? yeahh!!!
     
 )
@@ -198,9 +146,9 @@
     
 ; override and run
 (load "ch5-regsim.scm") 
-(define make-new-machine make-new-machine-5.11c) 
+(define make-new-machine-regsim make-new-machine) (define make-new-machine make-new-machine-5.11c) 
 (define make-save-regsim make-save) (define make-save make-save-5.11c) 
-(define make-restore make-restore-5.11c) 
+(define make-restore-regsim make-restore) (define make-restore make-restore-5.11c) 
 (test-5.11c)
 
 ; i think i'm finally starting to understand the mit/scheme philosophy
