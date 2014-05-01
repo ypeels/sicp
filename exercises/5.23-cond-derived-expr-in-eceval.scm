8027d46dd8dca65051d58f02b71a0cc3680d3e78
Jonathan Chen (work) <ypeels@live.com>
2014-05-01 12:49:21 -0500

    Exercise 5.23 - cond in EC-Eval, as a derived expression. by cheating and bringing in (cond->if) as a 'primitive', this winds up being only a few ASSEMBLY lines

--- exercises/5.23-cond-derived-expr-in-eceval.scm
+++ exercises/5.23-cond-derived-expr-in-eceval.scm
@@ -3,13 +3,16 @@
 
 (define eceval-cond-text-5.23 '(
     
-    
-    
-    ev-cond
-    
+ev-cond
 
-        (assign val (const "hello world\n"))
-        (goto (label signal-error))
+    ;(assign val (const "hello world\n"))
+    ;(goto (label signal-error))
+    
+    ; (eval (cond->if expr) env)
+    (assign expr (op cond->if) (reg expr))
+    ;(save continue) ; is this needed? probably harmful...?
+    (goto (label eval-dispatch))
+        ; is this IT??
         
 ))
 
@@ -17,6 +20,7 @@
 
     ; no, this had to be hard coded into ch5-eceval.scm, to allow "backwards compatibility" with the default eceval object
     ;(append! eceval-operations (list (list 'cond? cond?))))
+    (append! eceval-operations (list (list 'cond->if cond->if)))
 
     (define eceval-5.23
         (make-eceval 
@@ -25,3 +29,5 @@
     (start eceval-5.23)
 )
 (define the-global-environment (setup-environment)) (test-5.23)
+
+; (define (f x) (cond (x 'yes) (else 'no)))
