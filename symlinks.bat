c57282c5ed7e63f602f7c26ed2b9e1e092435896
Jonathan Chen (work) <ypeels@live.com>
2014-05-02 10:21:33 -0500

    Batch files to generate symbolic links

---
+++ symlinks.bat
@@ -0,0 +1,46 @@
+@echo off
+
+rem mklink requires Administrator privileges, but then cmd starts in C:\Windows\system32
+set SICP_ROOT=F:\Users\Jonathan\Documents\Qt\Projects-vc2008\sicp
+call :checkExists "%SICP_ROOT%"
+cd /d "%SICP_ROOT%"
+
+set TARGETS=symlink-targets.txt
+call :checkExists "%TARGETS%"
+
+setlocal enabledelayedexpansion
+for /f "tokens=1-3" %%i in (%TARGETS%) do (
+    pushd %%i
+    set TARGET_FILE=%%k
+    
+    rem http://www.computing.net/answers/programming/batch-file-replace-forward-slash/15589.html
+    call :makeSymlink %%j !TARGET_FILE:/=\!
+    
+    popd
+)
+
+echo Goodbye world
+pause
+exit /b
+
+
+
+:makeSymlink
+    call :checkExists %2    
+    if not exist %1 (
+        mklink %1 %2
+    ) else (
+        echo Skipping %CD%\%1 - already exists! 
+    )
+    rem echo "Want to create link %1 ==> %2"
+exit /b
+
+
+
+:checkExists
+    if not exist %1 (
+        echo Error - file not found: %1
+        pause
+        exit
+    )
+exit /b
---
+++ symlinks.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+checkExists() {
+    if [ ! -e "$1" ]; then #&& ![ -d "$1"] ]; then - how do you do compound conditions? meh
+        echo ERROR from shell.sh: $1 not found
+        pause
+        exit 1
+    fi
+}
+
+targets=symlink-targets.txt
+checkExists $targets
+
+# http://www.bashguru.com/2010/05/how-to-read-file-line-by-line-in-shell.html
+# http://stackoverflow.com/questions/918886/how-do-i-split-a-string-on-a-delimiter-in-bash
+# http://superuser.com/questions/194668/grep-to-find-files-that-contain-m-windows-carriage-return
+# a surprising amount of hassle to do something supported directly by "for" in DOS
+cat $targets | tr -d $'\r' | while read destDir destFile sourcePath; do 
+    
+    checkExists $destDir
+    pushd $destDir > /dev/null
+    
+        checkExists $sourcePath    
+        if [ -e $destFile ]; then
+            echo Skipping $(pwd)/$destFile - already exists
+        else
+            #echo Want to link $sourcePath to $destFile
+            ln -sv $sourcePath $destFile        
+        fi
+    
+    popd > /dev/null
+done
+
