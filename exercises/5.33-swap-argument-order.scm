ea2851cf91a7d7ce90ce2a69224f037bd8f12ce4
Jonathan Chen (netbook) <ypeels@live.com>
2014-05-03 10:06:59 -0500

    first thoughts

---
+++ exercises/5.33-swap-argument-order.scm
@@ -0,0 +1,6 @@
+; I guess output the object code to file as in 5.35, and analyze by diff?
+; should just be ordering differences
+; how am i supposed to run this code?? 
+    ; should i come back and finish this after reading 5.5.7?
+    ; should i look at "load-eceval-compiler"?
+    ; either way, i'm not doing this on my tiny netbook screen at howdy honda...
