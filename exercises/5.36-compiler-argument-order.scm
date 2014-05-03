15925df76237e8ddd7f205fd042f90ecd24b6f28
Jonathan Chen (home) <ypeels@live.com>
2014-05-03 16:03:10 -0500

    Exercise 5.36 - operand order of evaluation in compiler

--- exercises/5.36-compiler-argument-order.scm
+++ exercises/5.36-compiler-argument-order.scm
@@ -5,8 +5,28 @@
 ; to modify this, you would override/rewrite construct-arglist p. 583 and its helper
     ; aha, you could just remove the "reverse" on p. 583. that's what makes things go right to left!
     ; i suppose if the (reverse) is expensive, efficiency would be improved.
+        ; meteorgan doesn't talk about efficiency at all: http://community.schemewiki.org/?sicp-ex-5.36
+        ; wow, l0stman REALLY overthought things: https://github.com/l0stman/sicp/blob/master/5.36.tex
 
 
 ; the 5.4.1 reference is on p. 553? seems kinda pointless
     ; i guess their point is that you can actually find mechanistic code for order of evaluation?
         ; you don't have to figure things out empirically, like in chapter 1
+
+        
+(define (construct-arglist operand-codes)
+  ;(let ((operand-codes (reverse operand-codes)))           ; <===== voila, some other order of evaluation.
+    (if (null? operand-codes)
+        (make-instruction-sequence '() '(argl)
+         '((assign argl (const ()))))
+        (let ((code-to-get-last-arg
+               (append-instruction-sequences
+                (car operand-codes)
+                (make-instruction-sequence '(val) '(argl)
+                 '((assign argl (op list) (reg val)))))))
+          (if (null? (cdr operand-codes))
+              code-to-get-last-arg
+              (preserving '(env)
+               code-to-get-last-arg
+               (code-to-get-rest-args
+                (cdr operand-codes)))))));)
