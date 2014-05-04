2d4312d6e60ed506e2e65e1f341f073d98df6a23
Jonathan Chen (home) <ypeels@live.com>
2014-05-04 18:08:44 -0500

    Exercise 5.39 - lexical-address-lookup and lexical-address-set! - with as much code shared as possible

--- exercises/5.39-lexical-address-lookup.scm
+++ exercises/5.39-lexical-address-lookup.scm
@@ -11,18 +11,40 @@
     
     ; refactor to a driver for (lexical-address-set!) - use (list-set!)
 (define (lexical-address-lookup address env)
+    (lexical-address-driver address env
+        (lambda (frame displacement)
+            (let ((val (list-ref frame displacement)))
+                (if (eq? val '*unassigned*)
+                    (error "Unassigned variable - LEXICAL-ADDRESS-LOOKUP" address env)
+                    val
+                )
+            )            
+        )
+    )
+)
+
+(define (lexical-address-set! address env val)
+    (lexical-address-driver address env
+        (lambda (frame displacement)
+            (list-set! frame displacement val)
+        )
+    )
+)
+
+(define (lexical-address-driver address env op)
 
     ; frame API from ch5-eceval-support.scm
     (define (iter frame displacement env)
         (cond
             ((null? env)
-                (error "Bad address? -- LEXICAL-ADDRESS-LOOKUP" address))
+                (error "Bad address? -- LEXICAL-ADDRESS-DRIVER" address))
             ((< 0 frame)
-                (error "Negative frame number -- LEXICAL-ADDRESS-LOOKUP" address))
+                (error "Negative frame number -- LEXICAL-ADDRESS-DRIVER" address))
             ((= 0 frame)
             
                 ; and return the value of the variable stored at the specified lexical address.
-                (list-ref (frame-values (first-frame env)) displacement))
+                ;(list-ref (frame-values (first-frame env)) displacement))
+                (op (frame-values (first-frame env)) displacement))
                 
             (else
                 (iter (- frame 1) displacement (enclosing-environment env)))
