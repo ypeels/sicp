e93f31d98139a32cf504a60291ba95b5d5010799
Jonathan Chen (home) <ypeels@live.com>
2014-05-04 11:46:52 -0500

    refactored driver, and now i think recursion is quite natural

--- exercises/5.38-open-coded-primitives.scm
+++ exercises/5.38-open-coded-primitives.scm
@@ -1,7 +1,7 @@
 (load "5.33-38-compiling-to-file.scm") ; and override below
 
 ; for (compile-and-go) testing
-;(load "ch5-compiler.scm")(load "load-eceval-compiler.scm")
+(load "ch5-compiler.scm")(load "load-eceval-compiler.scm")
 
 ; Spread-arguments should take an operand list and compile the given operands targeted 
 ; to successive argument registers.
@@ -45,62 +45,88 @@
 
 ; For each of the primitive procedures =, *, -, and +, 
 ; takes a combination with that operator, together with a target and a linkage descriptor,
-(define (compile-open-code expr target linkage operation null-value)
-    (let ((arguments (operands expr)))
-        (cond
-            ((no-operands? arguments)
-                (compile null-value target linkage))
-            ((no-operands? (rest-operands arguments))
-                (compile (first-operand arguments) target linkage))
-            ((= 2 (length arguments))
+(define (compile-open-code arguments target linkage operation null-value)
+    (cond
+        ((no-operands? arguments)
+            (compile null-value target linkage))
+        ((no-operands? (rest-operands arguments))
+            (compile (first-operand arguments) target linkage))
+        ((= 2 (length arguments))
 
-                (end-with-linkage linkage
-                    (append-instruction-sequences
-                    
-                        ; and produces code to spread the arguments into the registers [arg1 and arg2]
-                        (spread-arguments arguments)
+            (end-with-linkage linkage
+                (append-instruction-sequences
+                
+                    ; and produces code to spread the arguments into the registers [arg1 and arg2]
+                    (spread-arguments arguments)
 
-                        ; and then perform the operation targeted to the given target with the given linkage
-                        (make-instruction-sequence
-                            '(arg1 arg2); needs
-                            (list target); modifies
-                            `(
-                                (assign
-                                    ,target
-                                    (op ,operation)
-                                    (reg arg1)  ; what a bad name - it looks exactly like argl...
-                                    (reg arg2)
-                                )
-                                
-                                ;(perform (op user-print) (const "\n("))
-                                ;(perform (op user-print) (const ,operation))
-                                ;(perform (op user-print) (const " "))
-                                ;(perform (op user-print) (reg arg1))
-                                ;(perform (op user-print) (const " "))
-                                ;(perform (op user-print) (reg arg2))
-                                ;(perform (op user-print) (const ") = "))
-                                ;(perform (op user-print) (reg ,target))
-                                
-                                
+                    ; and then perform the operation targeted to the given target with the given linkage
+                    (make-instruction-sequence
+                        '(arg1 arg2); needs
+                        (list target); modifies
+                        `(
+                            (assign
+                                ,target
+                                (op ,operation)
+                                (reg arg1)  ; what a bad name - it looks exactly like argl...
+                                (reg arg2)
                             )
+                            
+                            ;(perform (op user-print) (const "\n("))
+                            ;(perform (op user-print) (const ,operation))
+                            ;(perform (op user-print) (const " "))
+                            ;(perform (op user-print) (reg arg1))
+                            ;(perform (op user-print) (const " "))
+                            ;(perform (op user-print) (reg arg2))
+                            ;(perform (op user-print) (const ") = "))
+                            ;(perform (op user-print) (reg ,target))
+                            
+                            
                         )
                     )
                 )
-            
             )
-            (else (error "Bad number of arguments" (length arguments)))
+        
         )
+        
+        ; Extend your code generators for + and * so that they can handle expressions with arbitrary numbers of operands.
+        ;  An expression with more than two operands will have to be compiled into a sequence of operations, each with only two inputs. 
+        ((and (> 2 (length arguments)) (memq operation '(+ *)))
+        
+            ; sounds like they want us to fix this at the COMPILE level?
+            ; l0stman used the PARSE level, but that seems like a cheap cop-out...
+            
+            ; first idea, but this seems like parse-level
+            ;(compile-open-code
+            ;    (list 
+            ;        (operator expr)
+            ;        (first-operand arguments)
+            ;        (compile
+            
+            
+            
+            ;`((assign ,target (op ,operation) arg1 arg2))
+            ; want arg2 to be a recursive result - so 
+                ; (compile-open-code (cons (operator target linkage operation null-value)
+                ; can't avoid parse-processing??
+            ; want arg1 to be (compile (first-operand arguments) 'arg1 'next)
+            ; throw (preserving) in there as appropriate?
+            
+            (error "empty stub")
+        )
+            
+        (else (error "Bad number of arguments" (length arguments)))
     )
 )
 
+
 (define (compile-plus expr target linkage)
-    (compile-open-code expr target linkage '+ '0))
+    (compile-open-code (operands expr) target linkage '+ '0))
 (define (compile-times expr target linkage)
-    (compile-open-code expr target linkage '* '1))
+    (compile-open-code (operands expr) target linkage '* '1))
 (define (compile-minus expr target linkage)
-    (compile-open-code expr target linkage '- '0))   ; actually, (-) crashes scheme, but whatever
+    (compile-open-code (operands expr) target linkage '- '0))   ; actually, (-) crashes scheme, but whatever
 (define (compile-equals expr target linkage)
-    (compile-open-code expr target linkage '= #t))
+    (compile-open-code (operands expr) target linkage '= #t))
 
 (define (plus? expr) (tagged-list? expr '+))
 (define (times? expr) (tagged-list? expr '*))
@@ -156,16 +182,16 @@
     
     
     ; used for testing, but you have to modify ch5-eceval-compiler.scm
-    ;(compile-and-go 
-    ;    '(define (f n)
-    ;      (if (= n 1)
-    ;          1
-    ;          (* (f (- n 1)) n)))
-    ;)
+    (compile-and-go 
+        '(define (f n)
+          (if (= n 1)
+              1
+              (* (f (- n 1)) n)))
+    )
     
     ;(compile-and-go '(+ (+ 1 2) (+ 3 4)))
     
-    (compile-and-go '(+ 1 2 3 4))
+    ;(compile-and-go '(+ 1 2 3 4))
         
     
 )
