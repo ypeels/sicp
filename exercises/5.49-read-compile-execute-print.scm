08088fb3a1fb507a45d098efd4dccd30f5500d20
Jonathan Chen (work) <ypeels@live.com>
2014-05-07 12:58:21 -0500

    cleaned up and submitted to wiki

--- exercises/5.49-read-compile-execute-print.scm
+++ exercises/5.49-read-compile-execute-print.scm
@@ -6,95 +6,94 @@
     ; prints the result in val
     ; uses the eceval-operations runtime (I think)
     ; maintains eceval's register set so that env, etc. 
-    
-    
-(define eceval-5.49 '*unassigned*)
-    
-(define the-global-environment '())
 
-(define (assemble-for-eceval-5.49 compiler-output)
-    
-    (for-each
-        (lambda (line) (display line) (newline))
-        (statements compiler-output)
-    )
-    
-
-    ; cf. 5.48 and (compile-and-go)
-    (assemble (statements compiler-output) eceval-5.49)
-)
-
-
-; same register set as eceval - which is what the compiler expects.
-(define eceval-registers-5.49 '(expr env val proc argl continue unev))
-
-    
-(define eceval-controller-text-5.49 '(
-
-; first parrot the outermost loop of eceval-compiler
-read-compile-exec-print-loop
-    (perform (op initialize-stack)) ; better name inspired by l0stman
-    (perform (op prompt-for-input) (const ";;; EC-Comp-Exec input:"))
-    (assign expr (op read))
-    (assign env (op get-global-environment))
-    ;(assign continue (label print-result)) ; moved to comp-exec-dispatch
-    (goto (label comp-exec-dispatch))
-print-result
-    (perform (op print-stack-statistics))
-    (perform (op announce-output) (const ";;; EC-Comp-Exec value:"))
-    (perform (op user-print) (reg val))
-    (goto (label read-compile-exec-print-loop))
-  
-  
-; basically, this entire exercise is just about writing these 3 new lines of assembly code!
+; basically, this entire exercise is just about writing 3 new lines of assembly code at comp-exec
     ; expr = scheme expression from user
     ; continue = print-result
-comp-exec-dispatch  
-    (assign val (op compile) (reg expr) (const val) (const return)) ; you DON'T quote val/return here. ehhhh?
-    (assign val (op assemble-for-eceval-5.49) (reg val))
-    (assign continue (label print-result)); moved here from main loop for clarity. unlike eceval, this exit point is unconditional
-    (goto (reg val)) ; will return linkage automagically handle everything?? yes!
-))
-
 
 ; another cursed existence with little to no utility
     ; 5.48 yielded a compiling interpreter - typing expressions is slower than compiling in batch
     ; 5.49 yielded an interpreting compiler - compiling expressions is (possibly?) slower than interpreting
 
-
-; all (op)'s needed by eceval-instructions-5.49, and nothing more? 
-    ; no, assembler needs lookup-variable-value, etc. runtime
-(define eceval-operations-5.49 '*unassigned*)
-(define (install-eceval-operations-5.49)
-    (set! eceval-operations-5.49 
-        (append
-            eceval-operations
-            (list
-                (list 'compile compile)
-                (list 'assemble-for-eceval-5.49 assemble-for-eceval-5.49)
-            )
-        )
-    )
-)
-
-
-
-(define (test-5.49)
-    (load "ch5-regsim.scm") ; for (make-machine) and (assemble)
-    (load "ch5-compiler.scm") ; for (compile)
-    (load "ch5-eceval-support.scm") ; for syntax and utility procedures
-    (load "ch5-eceval-compiler.scm") ; for operation list, needed by assembler
+; can i do this by using ONLY the (op)'s needed by eceval-instructions-5.49, and nothing more? 
+    ; no, assembler needs lookup-variable-value, etc. runtime    
     
+; meh, this is in ch5-eceval-compiler.scm, which i needed to (load) for eceval-operations
+;(define the-global-environment '())  
+
+; huh? stack statistics are not working??
+    ; oh, you have to RUN a procedure that has pushes
+    ; defining factorial won't cause any push/pops
+    
+; removed development comments and code for uploading to wiki
+    ; see previous Git commit
+
+    
+; everything below uploaded to http://community.schemewiki.org/?sicp-ex-5.49
+; pity i forsook my idiosyncratic (set!) etc. usages
+    ; but what are the odds i'll ever (load) this file into ANYTHING??
+    ; one more to go!!!
+   
+(load "ch5-regsim.scm") ; for (make-machine) and (assemble)
+(load "ch5-compiler.scm") ; for (compile)
+(load "ch5-eceval-support.scm") ; for syntax and utility procedures
+(load "ch5-eceval-compiler.scm") ; for operation list, needed by assembler
+
+; return linkage makes compiled code return to print-result below
+(define (compile-and-assemble expr)
+    (assemble   
+        (statements (compile expr 'val 'return))
+        ec-comp-exec))
+
+; compiler expects same register set as eceval
+(define ec-comp-exec-registers
+    '(expr env val proc argl continue unev))
+    
+(define ec-comp-exec-operations 
+    (append 
+        eceval-operations ; from ch5-eceval-compiler.scm
+        (list (list 'compile-and-assemble compile-and-assemble))))    
+    
+    
+    
+(define ec-comp-exec-controller-text '(
+
+; main loop same as eceval
+read-compile-exec-print-loop
+    (perform (op initialize-stack)) 
+    (perform (op prompt-for-input) (const ";;; EC-Comp-Exec input:"))
+    (assign expr (op read))
+    (assign env (op get-global-environment))
+    (assign continue (label print-result)) 
+    (goto (label compile-and-execute)) ; ** label name changed
+print-result
+    (perform (op print-stack-statistics))
+    (perform (op announce-output) (const ";;; EC-Comp-Exec value:"))
+    (perform (op user-print) (reg val))
+    (goto (label read-compile-exec-print-loop))
+
+    
+; the entirety of the new machine! as per the problem statement, 
+; all complexity is deferred to "primitives" (compile) and (assemble)
+compile-and-execute
+    (assign val (op compile-and-assemble) (reg expr))
+    (goto (reg val))
+    
+))
+
+
+
+(define ec-comp-exec (make-machine
+    ec-comp-exec-registers
+    ec-comp-exec-operations 
+    ec-comp-exec-controller-text))
+
+(define (start-ec-comp-exec)
     (set! the-global-environment (setup-environment))
-    (install-eceval-operations-5.49)
-    (set! eceval-5.49 (make-machine
-        eceval-registers-5.49
-        eceval-operations-5.49
-        eceval-controller-text-5.49
-    ))
-    
-    (eceval-5.49 'start)
+    (ec-comp-exec 'start)
 )
-(test-5.49)
+
+(start-ec-comp-exec)
+
         
         
