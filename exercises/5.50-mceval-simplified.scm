7438e3cdf37d4f408c1e22e23d93430c1856b657
Jonathan Chen (work) <ypeels@live.com>
2014-05-07 13:19:16 -0500

    expunged exp keyword

--- exercises/5.50-mceval-simplified.scm
+++ exercises/5.50-mceval-simplified.scm
@@ -16,25 +16,25 @@
 
 ;;;SECTION 4.1.1
 
-(define (eval exp env)
-  (cond ((self-evaluating? exp) exp)
-        ((variable? exp) (lookup-variable-value exp env))
-        ((quoted? exp) (text-of-quotation exp))
-        ((assignment? exp) (eval-assignment exp env))
-        ((definition? exp) (eval-definition exp env))
-        ((if? exp) (eval-if exp env))
-        ((lambda? exp)
-         (make-procedure (lambda-parameters exp)
-                         (lambda-body exp)
+(define (eval expr env)
+  (cond ((self-evaluating? expr) expr)
+        ((variable? expr) (lookup-variable-value expr env))
+        ((quoted? expr) (text-of-quotation expr))
+        ((assignment? expr) (eval-assignment expr env))
+        ((definition? expr) (eval-definition expr env))
+        ((if? expr) (eval-if expr env))
+        ((lambda? expr)
+         (make-procedure (lambda-parameters expr)
+                         (lambda-body expr)
                          env))
-        ((begin? exp) 
-         (eval-sequence (begin-actions exp) env))
-        ((cond? exp) (eval (cond->if exp) env))
-        ((application? exp)
-         (apply (eval (operator exp) env)
-                (list-of-values (operands exp) env)))
+        ((begin? expr) 
+         (eval-sequence (begin-actions expr) env))
+        ((cond? expr) (eval (cond->if expr) env))
+        ((application? expr)
+         (apply (eval (operator expr) env)
+                (list-of-values (operands expr) env)))
         (else
-         (error "Unknown expression type -- EVAL" exp))))
+         (error "Unknown exprression type -- EVAL" expr))))
 
 (define (apply procedure arguments)
   (cond ((primitive-procedure? procedure)
@@ -51,102 +51,102 @@
           "Unknown procedure type -- APPLY" procedure))))
 
 
-(define (list-of-values exps env)
-  (if (no-operands? exps)
+(define (list-of-values exprs env)
+  (if (no-operands? exprs)
       '()
-      (cons (eval (first-operand exps) env)
-            (list-of-values (rest-operands exps) env))))
+      (cons (eval (first-operand exprs) env)
+            (list-of-values (rest-operands exprs) env))))
 
-(define (eval-if exp env)
-  (if (true? (eval (if-predicate exp) env))
-      (eval (if-consequent exp) env)
-      (eval (if-alternative exp) env)))
+(define (eval-if expr env)
+  (if (true? (eval (if-predicate expr) env))
+      (eval (if-consequent expr) env)
+      (eval (if-alternative expr) env)))
 
-(define (eval-sequence exps env)
-  (cond ((last-exp? exps) (eval (first-exp exps) env))
-        (else (eval (first-exp exps) env)
-              (eval-sequence (rest-exps exps) env))))
+(define (eval-sequence exprs env)
+  (cond ((last-exp? exprs) (eval (first-exp exprs) env))
+        (else (eval (first-exp exprs) env)
+              (eval-sequence (rest-exps exprs) env))))
 
-(define (eval-assignment exp env)
-  (set-variable-value! (assignment-variable exp)
-                       (eval (assignment-value exp) env)
+(define (eval-assignment expr env)
+  (set-variable-value! (assignment-variable expr)
+                       (eval (assignment-value expr) env)
                        env)
   'ok)
 
-(define (eval-definition exp env)
-  (define-variable! (definition-variable exp)
-                    (eval (definition-value exp) env)
+(define (eval-definition expr env)
+  (define-variable! (definition-variable expr)
+                    (eval (definition-value expr) env)
                     env)
   'ok)
 
 ;;;SECTION 4.1.2
 
-(define (self-evaluating? exp)
-  (cond ((number? exp) true)
-        ((string? exp) true)
+(define (self-evaluating? expr)
+  (cond ((number? expr) true)
+        ((string? expr) true)
         (else false)))
 
-(define (quoted? exp)
-  (tagged-list? exp 'quote))
+(define (quoted? expr)
+  (tagged-list? expr 'quote))
 
-(define (text-of-quotation exp) (cadr exp))
+(define (text-of-quotation expr) (cadr expr))
 
-(define (tagged-list? exp tag)
-  (if (pair? exp)
-      (eq? (car exp) tag)
+(define (tagged-list? expr tag)
+  (if (pair? expr)
+      (eq? (car expr) tag)
       false))
 
-(define (variable? exp) (symbol? exp))
+(define (variable? expr) (symbol? expr))
 
-(define (assignment? exp)
-  (tagged-list? exp 'set!))
+(define (assignment? expr)
+  (tagged-list? expr 'set!))
 
-(define (assignment-variable exp) (cadr exp))
+(define (assignment-variable expr) (cadr expr))
 
-(define (assignment-value exp) (caddr exp))
+(define (assignment-value expr) (caddr expr))
 
 
-(define (definition? exp)
-  (tagged-list? exp 'define))
+(define (definition? expr)
+  (tagged-list? expr 'define))
 
-(define (definition-variable exp)
-  (if (symbol? (cadr exp))
-      (cadr exp)
-      (caadr exp)))
+(define (definition-variable expr)
+  (if (symbol? (cadr expr))
+      (cadr expr)
+      (caadr expr)))
 
-(define (definition-value exp)
-  (if (symbol? (cadr exp))
-      (caddr exp)
-      (make-lambda (cdadr exp)
-                   (cddr exp))))
+(define (definition-value expr)
+  (if (symbol? (cadr expr))
+      (caddr expr)
+      (make-lambda (cdadr expr)
+                   (cddr expr))))
 
-(define (lambda? exp) (tagged-list? exp 'lambda))
+(define (lambda? expr) (tagged-list? expr 'lambda))
 
-(define (lambda-parameters exp) (cadr exp))
-(define (lambda-body exp) (cddr exp))
+(define (lambda-parameters expr) (cadr expr))
+(define (lambda-body expr) (cddr expr))
 
 (define (make-lambda parameters body)
   (cons 'lambda (cons parameters body)))
 
 
-(define (if? exp) (tagged-list? exp 'if))
+(define (if? expr) (tagged-list? expr 'if))
 
-(define (if-predicate exp) (cadr exp))
+(define (if-predicate expr) (cadr expr))
 
-(define (if-consequent exp) (caddr exp))
+(define (if-consequent expr) (caddr expr))
 
-(define (if-alternative exp)
-  (if (not (null? (cdddr exp)))
-      (cadddr exp)
+(define (if-alternative expr)
+  (if (not (null? (cdddr expr)))
+      (cadddr expr)
       'false))
 
 (define (make-if predicate consequent alternative)
   (list 'if predicate consequent alternative))
 
 
-(define (begin? exp) (tagged-list? exp 'begin))
+(define (begin? expr) (tagged-list? expr 'begin))
 
-(define (begin-actions exp) (cdr exp))
+(define (begin-actions expr) (cdr expr))
 
 (define (last-exp? seq) (null? (cdr seq)))
 (define (first-exp seq) (car seq))
@@ -160,18 +160,18 @@
 (define (make-begin seq) (cons 'begin seq))
 
 
-(define (application? exp) (pair? exp))
-(define (operator exp) (car exp))
-(define (operands exp) (cdr exp))
+(define (application? expr) (pair? expr))
+(define (operator expr) (car expr))
+(define (operands expr) (cdr expr))
 
 (define (no-operands? ops) (null? ops))
 (define (first-operand ops) (car ops))
 (define (rest-operands ops) (cdr ops))
 
 
-(define (cond? exp) (tagged-list? exp 'cond))
+(define (cond? expr) (tagged-list? expr 'cond))
 
-(define (cond-clauses exp) (cdr exp))
+(define (cond-clauses expr) (cdr expr))
 
 (define (cond-else-clause? clause)
   (eq? (cond-predicate clause) 'else))
@@ -180,8 +180,8 @@
 
 (define (cond-actions clause) (cdr clause))
 
-(define (cond->if exp)
-  (expand-clauses (cond-clauses exp)))
+(define (cond->if expr)
+  (expand-clauses (cond-clauses expr)))
 
 (define (expand-clauses clauses)
   (if (null? clauses)
