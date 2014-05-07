4aca6d0f70bd9f30559d640126c1f0bd2c2645b5
Jonathan Chen (work) <ypeels@live.com>
2014-05-07 10:58:14 -0500

    before deleting development code

--- exercises/5.48-compile-and-run.scm
+++ exercises/5.48-compile-and-run.scm
@@ -1,9 +1,13 @@
 ; this should be pretty easy, right?
 ; just do something similar to (compile-and-go), and make it accessible to eceval
+    ; ugh, grappled with syntax for QUITE a while
+    
+; l0stman, on the other hand, just calls (start eceval) from within (compile-and-run)
+    ; impressive that this works, but i worry about garbage buildup...
 
 
-
-(define (compile-and-run expression)
+;(load "5.33-38-compiling-to-file.scm")
+(define (compile-and-run-development expression)
     (let* ( ;(pc ((eceval 'get-register) 'pc))
             ;(continue ((eceval 'get-register) 'continue))
             ;(printres ((eceval 'get-register) 'printres))
@@ -62,32 +66,60 @@
         'compile-and-run-done
     )
 )
+
+
+
+; condensed version for uploading to wiki
+; http://community.schemewiki.org/?sicp-ex-5.48
+
+; this code will get executed by primitive-apply in ch5-eceval-compiler.scm
+; specifically, by (apply-primitive-procedure)
+(define (compile-and-run expression)
+    (let ((stack (eceval 'stack))
+          (instructions
+            (assemble
+                (append
+                    (statements (compile expression 'val 'next))
+                    
+                    ; print the result after executing statements
+                    '((goto (reg printres))))
+                eceval)))
+            
+        ; get rid of old value of continue
+        ; this is optional, because (initialize-stack) will 
+        ; clear the stack after print-result
+        (stack 'pop) 
         
+        ; the next 2 commands in primitive-apply are:
+        ; (restore continue)
+        ; (goto (reg continue))
+        ; this forces eceval to jump to and execute instructions
+        ((stack 'push) instructions)))
         
-        
-        
+
+; --------------------------------------------------        
         
 (define (test-5.48)
     (load "ch5-compiler.scm")
     (load "load-eceval-compiler.scm")
-    ;(load "5.33-38-compiling-to-file.scm")
     
+    ; add and initialize new register printres to expose label print-result
+    ; cf. compadd from 5.47
     (set! eceval (make-machine
         (cons 'printres eceval-compiler-register-list)
-        eceval-operations   ; these are the procedures accessible via (op) in assembly code
+        eceval-operations   ; procedures accessible via (op) in asm code
         (cons 
-            '(assign printres (label print-result)) ; hack to print result after compile-and-run
-            eceval-compiler-main-controller-text 
-        )
-    ))
+            '(assign printres (label print-result)) 
+            eceval-compiler-main-controller-text)))
     
-    ; these are procedures accessible at the EC-Eval prompt
-    (append! primitive-procedures (list (list 'compile-and-run compile-and-run)))
-    
+    ; procedures accessible at the EC-Eval prompt
+    (append! primitive-procedures 
+        (list (list 'compile-and-run compile-and-run)))    
     
     (start-eceval)
 )
-;(test-5.48)
+(test-5.48)
+
 ; ;;; EC-Eval input:
 ; (compile-and-run '(define (factorial n) (if (= n 1) 1 (* (factorial (- n 1)) n))))
 ; ;;; EC-Eval value:
